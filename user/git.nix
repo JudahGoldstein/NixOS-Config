1859d8a8708b60c8da3fea093067bf6043bee515
Judah <yehudah.lev+github@gmail.com>
2024-04-18 15:40:43 -0400

    figuring out a git frontend

--- user/git.nix
+++ user/git.nix
@@ -1,20 +1,28 @@
 { config, pkgs, ... }:
 {
-	programs.git = {
-		enable = true;
-		userName  = "Judah";
-		userEmail = "yehudah.lev+github@gmail.com";
-		extraConfig = {
-			init.defaultBranch = "main";
-			push.autoSetupRemote = true;
+	programs={
+		git = {
+			enable = true;
+			userName  = "Judah";
+			userEmail = "yehudah.lev+github@gmail.com";
+			extraConfig = {
+				init.defaultBranch = "main";
+				push.autoSetupRemote = true;
+			};
+		};
+	
+		gh = {
+			enable = true;
+			gitCredentialHelper ={
+			 enable = true;
+			 hosts = ["https://github.com"];
+			};	
+		};
+
+		lazygit = {
+			enable = true;
 		};
 	};
-	
-	programs.gh = {
-		enable = true;
-		gitCredentialHelper ={
-		 enable = true;
-		 hosts = ["https://github.com"];
-		};	
-	};
+
+		home.packages = with pkgs; [pkgs.gitkraken];
 }
