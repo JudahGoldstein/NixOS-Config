5e3f81307db677c27713ced64a7ec090d0790576
Judah Goldstein <yehudah.lev@gmail.com>
2024-04-24 17:01:42 -0400

    add openbox

--- system/desktop/desktop.nix
+++ system/desktop/desktop.nix
@@ -4,10 +4,12 @@
     [
     ./gnome.nix
     ./kde.nix
+    ./openbox.nix
     ];
 
     gnome.enable = lib.mkDefault false;
     kde.enable = lib.mkDefault false;
+    openbox.enable = false;
 
     services.xserver.enable = true;
 }
