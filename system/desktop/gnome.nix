424fdc3e0e5b3ec5732eb4e62040fe7036b26dfa
Judah Goldstein <yehudah.lev@gmail.com>
2024-04-24 18:44:19 -0400

    some dconf tweaking

--- system/desktop/gnome.nix
+++ system/desktop/gnome.nix
@@ -2,8 +2,13 @@
 {
     options = {
         gnome.enable = lib.mkEnableOption "gnome";
+        gnome.terminal = lib.mkTextOption {
+            default = "kitty";
+            example = "gnome-terminal";
+            description = "The terminal emulator to use.";
+        };
     };
-    config = lib.mkIf config.gnome.enable{
+    config = lib.mkIf config.gnome.enable {
         services.xserver.displayManager.gdm.enable = true;
         services.xserver.desktopManager.gnome.enable = true;
         environment.gnome.excludePackages = with pkgs.gnome;
@@ -31,5 +36,42 @@
         pkgs.gnome-console
         pkgs.gnome-tour
         ];
+    };
+    home-manager.users.judah = {
+        dconf = {
+            enable = true;
+            settings= {
+                "org/gnome/desktop/interface" = {
+                    color-scheme = "prefer-dark";
+                    gtk-theme = "Adwaita-dark";
+                    clock-show-seconds = true;
+                    clock-show-weekday = true;
+                };
+                "org/gnome/desktop/peripherals/keyboard".numlock-state = true;
+                "org/gnome/desktop/privacy".report-technical-problems = false;
+                "org/gnome/desktop/wm/keybindings"={
+                    move-to-monitor-down = "@as []";
+                    move-to-monitor-left = "@as []";
+                    move-to-monitor-right = "@as []";
+                    move-to-monitor-up = "@as []";
+                    move-to-workspace-left = "['<Shift><Super>Left']";
+                    move-to-workspace-right = "['<Shift><Super>Right']";
+                    switch-to-workspace-left = "['<Super>Left']";
+                    switch-to-workspace-right = "['<Super>Right']";
+                };
+                "org/gnome/mutter/keybindings" = {
+                    toggle-tiled-left = "@as []";
+                    toggle-tiled-right = "@as []";
+                };
+                "org/gnome/desktop/wm/preferences".button-layout = "appmenu:minimize,maximize,close";
+                "org/gnome/nautilus/compression".default-compression-format = "'7z'";
+
+                "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+                    binding = "<Super>Return";
+                    command = gnome.terminal;
+                    name = "terminal";
+                };
+            };
+        };
     };
 }
