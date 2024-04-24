5a08121276828b4fe1f7f045f36ffc102b3464d3
Judah Goldstein <yehudah.lev@gmail.com>
2024-04-24 19:06:17 -0400

    dconf tweaking

--- home.nix
+++ home.nix
@@ -28,6 +28,8 @@ in
         TERM = "kitty";
     };
 
+    gnome.enable = true;
+
     home.stateVersion = "23.11"; # Please read the comment before changing.
 
     programs.home-manager.enable = true;
