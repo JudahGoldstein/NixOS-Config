69a901a48d59b7dc0e982fd57c065b9bef991f7e
Judah Goldstein <yehudah.lev@gmail.com>
2024-04-24 17:11:17 -0400

    add xfce

--- system/desktop/desktop.nix
+++ system/desktop/desktop.nix
@@ -5,11 +5,13 @@
     ./gnome.nix
     ./kde.nix
     ./openbox.nix
+    ./xfce.nix
     ];
 
     gnome.enable = lib.mkDefault false;
     kde.enable = lib.mkDefault false;
     openbox.enable = lib.mkDefault false;
+    xfce.enable = lib.mkDefault false;
 
     services.xserver.enable = true;
 }
