0c4a1d48dd9751c34c82e101b28ce9460a138415
Judah Goldstein <yehudah.lev@gmail.com>
2024-04-24 17:38:39 -0400

    kde tweaking

--- system/desktop/kde.nix
+++ system/desktop/kde.nix
@@ -6,7 +6,16 @@
     config =  lib.mkIf config.kde.enable {
         services.displayManager.sddm.enable = true;
         services.desktopManager.plasma6.enable = true;
-        services.xserver.displayManager.defaultSession = "plasmax11";
-        services.xserver.displayManager.sddm.wayland.enable = false;
+
+    services.xserver.displayManager.defaultSession = "plasmax11";
+    services.xserver.displayManager.sddm.wayland.enable = false;
+    environment.plasma6.excludePackages = with pkgs.kdePackages;
+    [
+    konsole
+    elisa
+    kwrite
+    kwalletmanager
+    ];
+
     };
 }
