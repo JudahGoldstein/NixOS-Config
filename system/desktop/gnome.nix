49692fb21503686551a2d74076ffc48c54efd3db
Judah Goldstein <yehudah.lev@gmail.com>
2024-04-24 17:56:48 -0400

    debloat gnome

--- system/desktop/gnome.nix
+++ system/desktop/gnome.nix
@@ -6,28 +6,28 @@
     config = lib.mkIf config.gnome.enable{
         services.xserver.displayManager.gdm.enable = true;
         services.xserver.desktopManager.gnome.enable = true;
+        environment.gnome.excludePackages = with pkgs.gnome;
+        [
+        eog
+        epiphany
+        gedit
+        simple-scan
+        totem
+        yelp
+        evince
+        file-roller
+        geary
+        seahorse
+        gnome-calculator
+        gnome-clocks
+        gnome-contacts
+        gnome-font-viewer
+        gnome-logs
+        gnome-maps
+        gnome-music
+        gnome-weather
+        pkgs.gnome-connections
+        gnome-terminal
+        ];
     };
-    environment.gnome.excludePackages = with pkgs.gnome;
-    [
-    eog
-    epiphany
-    gedit
-    simple-scan
-    totem
-    yelp
-    evince
-    file-roller
-    geary
-    seahorse
-    gnome-calculator
-    gnome-clocks
-    gnome-contacts
-    gnome-font-viewer
-    gnome-logs
-    gnome-maps
-    gnome-music
-    gnome-weather
-    pkgs.gnome-connections
-    gnome-terminal
-    ];
 }
