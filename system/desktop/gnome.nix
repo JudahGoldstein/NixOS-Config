7e6c5e9f8fdcf5085a944619c91c8d05961e5e3e
Judah Goldstein <yehudah.lev@gmail.com>
2024-04-24 16:36:07 -0400

    trying out conditionals

--- system/desktop/gnome.nix
+++ system/desktop/gnome.nix
@@ -1,5 +1,14 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+    options = {
+        gnome.enable =
+            lib.mkEnableOption "gnome" {
+                description = "Enable GNOME desktop environment";
+                default = false;
+            };
+    };
+    config = lib.mkIf config.gnome.enable{
+        services.xserver.displayManager.gdm.enable = true;
+        services.xserver.desktopManager.gnome.enable = true;
+    };
 }
--- system/desktop/kde.nix
+++ system/desktop/kde.nix
@@ -1,5 +1,14 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
-    services.displayManager.sddm.enable = true;
-    services.desktopManager.plasma6.enable = true;
+    options = {
+        kde.enable =
+            lib.mkEnableOption "kde" {
+                description = "Enable KDE desktop environment";
+                default = false;
+            };
+    };
+    config =  lib.mkIf config.kde.enable {
+        services.displayManager.sddm.enable = true;
+        services.desktopManager.plasma6.enable = true;
+    };
 }
