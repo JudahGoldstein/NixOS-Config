b2a59c92030abe794fd7cb864c69308eb7ab0048
Judah Goldstein <yehudah.lev@gmail.com>
2024-04-24 19:34:02 -0400

    dconf tweaking

--- user/desktop/gnome.nix
+++ user/desktop/gnome.nix
@@ -25,15 +25,10 @@ in
                 "org/gnome/desktop/peripherals/keyboard".numlock-state = true;
                 "org/gnome/desktop/privacy".report-technical-problems = false;
                 "org/gnome/desktop/wm/keybindings" = {
-                    move-to-monitor-down = "@as []";
-                    move-to-monitor-left = "@as []";
-                    move-to-monitor-right = "@as []";
-                    move-to-monitor-up = "@as []";
-                    switch-to-monitor-down = "@as []";
-                    switch-to-monitor-left = "@as []";
-                    switch-to-monitor-right = "@as []";
-                    switch-to-monitor-up = "@as []";
-
+                    move-to-monitor-down = "'@as []'";
+                    move-to-monitor-left = "'@as []'";
+                    move-to-monitor-right = "'@as []'";
+                    move-to-monitor-up = "'@as []'";
                     move-to-workspace-left = ["<Shift><Super>Left"];
                     move-to-workspace-right = ["<Shift><Super>Right"];
                     switch-to-workspace-left = ["<Super>Left"];
