6a1f4e750c5ffe6a4229bd129b9d98e0bd318c23
Judah Goldstein <yehudah.lev@gmail.com>
2024-04-24 17:07:04 -0400

    add openbox

--- system/desktop/desktop.nix
+++ system/desktop/desktop.nix
@@ -9,7 +9,7 @@
 
     gnome.enable = lib.mkDefault false;
     kde.enable = lib.mkDefault false;
-    openbox.enable = false;
+    openbox.enable = lib.mkDefault false;
 
     services.xserver.enable = true;
 }
--- system/desktop/openbox.nix
+++ system/desktop/openbox.nix
@@ -5,6 +5,7 @@
     };
     config =  lib.mkIf config.openbox.enable {
         services.displayManager.sddm.enable = true;
-        services.xserver.windowManager.openbox.enable = true;  };
+        services.xserver.windowManager.openbox.enable = true;
+    };
 }
 
