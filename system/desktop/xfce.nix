e76b5151e6e5f622e2313c6f08b429d1351f0321
Judah Goldstein <yehudah.lev@gmail.com>
2024-04-24 17:11:17 -0400

    add xfce

---
+++ system/desktop/xfce.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, lib, ... }:
+{
+    options = {
+        xfce.enable = lib.mkEnableOption "xfce";
+    };
+    config =  lib.mkIf config.xfce.enable {
+        services.displayManager.sddm.enable = true;
+        services.desktopManager.xfce.enable = true;
+    };
+}
