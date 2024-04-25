4ba80314e3d37c8a46fe4122716e3829a4850045
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-04-25 15:10:17 -0400

    some more gnome tweaking and setting the session to x11 for plover support

--- system/desktop/gnome.nix
+++ system/desktop/gnome.nix
@@ -4,8 +4,13 @@
         gnome.enable = lib.mkEnableOption "gnome";
     };
     config = lib.mkIf config.gnome.enable {
-        services.xserver.displayManager.gdm.enable = true;
-        services.xserver.desktopManager.gnome.enable = true;
+        services.xserver.displayManager = {
+	        gdm={
+		        enable = true;
+		        wayland = false;
+	        };
+        	gnome.enable = true;
+        };
         environment.gnome.excludePackages = with pkgs.gnome;
         [
         eog
