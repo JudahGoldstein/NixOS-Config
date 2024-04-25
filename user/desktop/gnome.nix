c9f5c0d6a955349605968690b601d2b92f1a4037
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-04-25 14:33:45 -0400

    dconf tweaking

--- user/desktop/gnome.nix
+++ user/desktop/gnome.nix
@@ -42,22 +42,17 @@ in
                 };
                 "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"];
                 "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-                	binding=["<Super>Return"];
+                	binding="<Super>Return";
                 	command="kitty";
                 	name="terminal";
-                }
+                };
                 "org/gnome/mutter" = {
-                	 dynamic-workspaces=true;
+                	 dynamic-workspaces = true;
                 	 edge-tiling = true;
                 };
-                "org/gtk/gtk4/settings/file-chooser".show-hidden = true
+                "org/gtk/gtk4/settings/file-chooser".show-hidden = true;
                 "org/gnome/desktop/wm/preferences".button-layout = "appmenu:minimize,maximize,close";
                 "org/gnome/nautilus/compression".default-compression-format = "'7z'";
-                "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-                    binding = "<Super>Return";
-                    command = terminal;
-                    name = "terminal";
-                };
             };
         };
     };
