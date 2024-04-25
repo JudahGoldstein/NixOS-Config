127aad9c17d877966b35383889775bdbc195343d
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-04-25 13:27:30 -0400

    Update bootloader.nix

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -1,17 +1,20 @@
 {config, pkgs, ...}:
 let 
-    device = "/dev/sda";
-    timeout = 3;
-    configurationLimit = 5;
+#    device = "/dev/sda";
+#    timeout = 3;
+#    configurationLimit = 5;
 in
 {
-    boot.loader = {
-        inherit timeout;
-        grub = {
-            enable = true;
-            inherit device;
-            useOSProber = false;
-            inherit configurationLimit;
-        };
-    };
+	boot.loader.systemd-boot.enable = true;
+  	boot.loader.efi.canTouchEfiVariables = true;
+
+#    boot.loader = {
+#        inherit timeout;
+#        grub = {
+#            enable = true;
+#            inherit device;
+#            useOSProber = false;
+#            inherit configurationLimit;
+#        };
+#    };
 }
