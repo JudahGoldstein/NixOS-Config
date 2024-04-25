a672133b9187d02071bc2a22e86319d3b42420a8
Judah <yehudah.lev+github@gmail.com>
2024-04-25 14:22:53 -0400

    make my git name more consistent

--- user/git.nix
+++ user/git.nix
@@ -2,7 +2,7 @@
 {
 	programs.git = {
 		enable = true;
-		userName  = "Judah";
+		userName  = "Judah Goldstein";
 		userEmail = "yehudah.lev+github@gmail.com";
 		extraConfig = {
 			init.defaultBranch = "main";
