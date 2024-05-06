393a39534375dde1963d1cf86bc6db466ee2dbc9
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-05-05 23:43:17 -0400

    add conditionals

---
+++ system/battery/battery.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, lib, ... }:
+with lib;
+{
+    imports =
+    [
+    ./power-profiles.nix
+    ./tpl.nix
+    ];
+
+    tpl.enable = mkDefault true;
+    power-profiles.enable = mkDefault false;
+}
