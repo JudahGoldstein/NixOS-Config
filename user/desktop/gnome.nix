b42bde9375a17f3d413c325d1efbc0ce4b5821fd
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-05-29 22:48:45 -0400

    modularization

--- user/desktop/gnome.nix
+++ user/desktop/gnome.nix
@@ -5,65 +5,63 @@ let
 in
 {
   options = {
-    gnome.enable = lib.mkEnableOption "gnome";
-    terminal = lib.mkOption {
-      type = lib.types.str;
-      default = "kitty";
-      example = "gnome-terminal";
-      description = "The terminal emulator to use.";
+    gnome.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable GNOME desktop environment.";
     };
   };
-  config =
-    lib.mkIf config.gnome.enable {
-      home.packages = with pkgs;
-        [
-          pkgs.gnome3.gnome-tweaks
-          pkgs.gnome-extension-manager
-        ];
-      dconf = {
-        enable = true;
-        settings = {
-          "org/gnome/desktop/interface" = {
-            color-scheme = "prefer-dark";
-            gtk-theme = "Adwaita-dark";
-            clock-show-seconds = true;
-            clock-show-weekday = true;
-            show-battery-percentage = true;
-          };
-          "org/gnome/desktop/peripherals/keyboard".numlock-state = true;
-          "org/gnome/desktop/privacy".report-technical-problems = false;
-          "org/gnome/desktop/wm/keybindings" = {
-            move-to-monitor-down = [ ];
-            move-to-monitor-left = [ ];
-            move-to-monitor-right = [ ];
-            move-to-monitor-up = [ ];
-            move-to-workspace-left = [ "<Shift><Super>Left" ];
-            move-to-workspace-right = [ "<Shift><Super>Right" ];
-            switch-to-workspace-left = [ "<Super>Left" ];
-            switch-to-workspace-right = [ "<Super>Right" ];
-          };
-          "org/gnome/mutter/keybindings" = {
-            toggle-tiled-left = [ ];
-            toggle-tiled-right = [ ];
-          };
-          "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
-          "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-            binding = "<Super>Return";
-            command = "kitty";
-            name = "terminal";
-          };
-          "org/gnome/mutter" = {
-            dynamic-workspaces = true;
-            edge-tiling = true;
-          };
-          "org/gnome/desktop/peripherals/touchpad" = {
-            natural-scroll = false;
-            two-finger-scrolling-enabled = true;
-          };
-          "org/gtk/gtk4/settings/file-chooser".show-hidden = true;
-          "org/gnome/desktop/wm/preferences".button-layout = "appmenu:minimize,maximize,close";
-          "org/gnome/nautilus/compression".default-compression-format = "7z";
+
+  config = lib.mkIf config.gnome.enable {
+    home.packages = with pkgs;
+      [
+        pkgs.gnome3.gnome-tweaks
+        pkgs.gnome-extension-manager
+      ];
+    dconf = {
+      enable = true;
+      settings = {
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          gtk-theme = "Adwaita-dark";
+          clock-show-seconds = true;
+          clock-show-weekday = true;
+          show-battery-percentage = true;
         };
+        "org/gnome/desktop/peripherals/keyboard".numlock-state = true;
+        "org/gnome/desktop/privacy".report-technical-problems = false;
+        "org/gnome/desktop/wm/keybindings" = {
+          move-to-monitor-down = [ ];
+          move-to-monitor-left = [ ];
+          move-to-monitor-right = [ ];
+          move-to-monitor-up = [ ];
+          move-to-workspace-left = [ "<Shift><Super>Left" ];
+          move-to-workspace-right = [ "<Shift><Super>Right" ];
+          switch-to-workspace-left = [ "<Super>Left" ];
+          switch-to-workspace-right = [ "<Super>Right" ];
+        };
+        "org/gnome/mutter/keybindings" = {
+          toggle-tiled-left = [ ];
+          toggle-tiled-right = [ ];
+        };
+        "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          binding = "<Super>Return";
+          command = "kitty";
+          name = terminal;
+        };
+        "org/gnome/mutter" = {
+          dynamic-workspaces = true;
+          edge-tiling = true;
+        };
+        "org/gnome/desktop/peripherals/touchpad" = {
+          natural-scroll = false;
+          two-finger-scrolling-enabled = true;
+        };
+        "org/gtk/gtk4/settings/file-chooser".show-hidden = true;
+        "org/gnome/desktop/wm/preferences".button-layout = "appmenu:minimize,maximize,close";
+        "org/gnome/nautilus/compression".default-compression-format = "7z";
       };
     };
+  };
 }
