e0032d00062029c1f5851abe030ea585048525aa
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-05-29 22:26:56 -0400

    better to just use tpl all the time

--- system/battery.nix
+++ system/battery.nix
@@ -3,34 +3,23 @@ with lib;
 {
   options =
     {
-      tlp.enable = {
+      battery.enable = {
         type = types.bool;
         default = true;
         description = "Enable TLP power management";
       };
-
-      power-profiles.enable = {
-        type = types.bool;
-        default = false;
-        description = "Enable power-profiles-daemon";
-      };
-
     };
 
-  config = {
-    powerManagement.enable = if ((config.tlp.enable == true) || (config.power-profiles.enable == true)) then true else false;
-    services.power-profiles-daemon.enable = if ((config.power-profiles.enable == true) && (config.tpl.enable == false)) == true then true else false;
-    services.tlp =
-      if (config.tlp.enable == true) then {
-        enable = true;
-        settings = {
-          CPU_SCALING_GOVERNOR_ON_AC = "performance";
-          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        };
-      } else {
-        enable = false;
+  config = lib.mkIf config.battery.enable {
+    powerManagement.enable = true;
+    services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       };
+    };
   };
 }
