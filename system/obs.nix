5613ce47fdb4b64162e8dceb56cf29b58feb1441
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-05-29 21:29:47 -0400

    modularize obs

--- system/obs.nix
+++ system/obs.nix
@@ -1,7 +1,17 @@
 { config, pkgs, lib, ... }:
 {
-  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
-  boot.kernelModules = [
-    "v4l2loopback"
-  ];
+  options = {
+    obs.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the OBS Studio streaming software kernel module for virtual video devices.";
+    };
+  };
+
+  config = {
+    boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+    boot.kernelModules = [
+      "v4l2loopback"
+    ];
+  };
 }
