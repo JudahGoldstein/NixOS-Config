042f49b40c655fa31ef1346fec719401e0dac841
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-05-29 23:01:51 -0400

    make optional

--- user/vscode.nix
+++ user/vscode.nix
@@ -1,18 +1,28 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
 {
+  options = {
+    vscode.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable Visual Studio Code";
+    };
+  };
 
-  home.packages = with pkgs;
-    [
-      # for python coding
-      python3Full
+  config = {
+    home.packages = with pkgs;
+      [
+        # for python coding
+        python3Full
 
-      # for nix coding
-      nil
-      nixpkgs-fmt
-    ];
+        # for nix coding
+        nil
+        nixpkgs-fmt
+      ];
 
-  programs = {
-    direnv.enable = true;
-    vscode.enable = true;
+    programs = {
+      direnv.enable = true;
+      vscode.enable = true;
+    };
   };
 }
