4ee2149b7cfbd23835b32d2f00b0df0114f0d870
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-05-29 21:02:13 -0400

    modularization

--- system/desktop/openbox.nix
+++ system/desktop/openbox.nix
@@ -1,9 +1,13 @@
 { config, pkgs, lib, ... }:
 {
   options = {
-    openbox.enable = lib.mkEnableOption "openbox";
+    openbox.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Openbox window manager";
+    };
   };
-  config = lib.mkIf config.openbox.enable {
+  config = lib.mkIf (config.openbox.enable == true) {
     services.displayManager.sddm.enable = true;
     services.xserver.windowManager.openbox.enable = true;
   };
