168e6e979fdfcf8557c27f049446a8076686a0da
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-05-29 22:31:03 -0400

    formatting

--- system/desktop/gnome.nix
+++ system/desktop/gnome.nix
@@ -8,39 +8,38 @@
     };
   };
 
-  config =
-    lib.mkIf (config.gnome.enable == true) {
-      services.xserver = {
-        displayManager.gdm = {
-          enable = true;
-          wayland = false;
-        };
-        desktopManager.gnome.enable = true;
+  config = lib.mkIf (config.gnome.enable == true) {
+    services.xserver = {
+      displayManager.gdm = {
+        enable = true;
+        wayland = false;
       };
-      environment.gnome.excludePackages = with pkgs.gnome;
-        [
-          eog
-          epiphany
-          pkgs.gedit
-          simple-scan
-          totem
-          yelp
-          evince
-          file-roller
-          geary
-          seahorse
-          gnome-calculator
-          gnome-clocks
-          gnome-contacts
-          gnome-font-viewer
-          gnome-logs
-          gnome-maps
-          gnome-music
-          gnome-weather
-          pkgs.gnome-connections
-          gnome-terminal
-          pkgs.gnome-console
-          pkgs.gnome-tour
-        ];
+      desktopManager.gnome.enable = true;
     };
+    environment.gnome.excludePackages = with pkgs.gnome;
+      [
+        eog
+        epiphany
+        pkgs.gedit
+        simple-scan
+        totem
+        yelp
+        evince
+        file-roller
+        geary
+        seahorse
+        gnome-calculator
+        gnome-clocks
+        gnome-contacts
+        gnome-font-viewer
+        gnome-logs
+        gnome-maps
+        gnome-music
+        gnome-weather
+        pkgs.gnome-connections
+        gnome-terminal
+        pkgs.gnome-console
+        pkgs.gnome-tour
+      ];
+  };
 }
