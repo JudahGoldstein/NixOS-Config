8c428194eb2a6763b1249ed61d2523489b2835c7
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-05-29 21:02:13 -0400

    modularization

--- system/desktop/xfce.nix
+++ system/desktop/xfce.nix
@@ -1,9 +1,13 @@
 { config, pkgs, lib, ... }:
 {
   options = {
-    xfce.enable = lib.mkEnableOption "xfce";
+    xfce.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the XFCE desktop environment.";
+    };
   };
-  config = lib.mkIf config.xfce.enable {
+  config = lib.mkIf (config.xfce.enable == true) {
     services.displayManager.sddm.enable = true;
     services.xserver.desktopManager.xfce.enable = true;
   };
