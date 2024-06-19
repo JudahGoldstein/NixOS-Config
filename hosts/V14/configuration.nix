5a30b271957aa2ece5c3bbbec2467bdb5cb1b379
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-06-19 16:19:38 -0400

    link loader and ssd trimming

--- hosts/V14/configuration.nix
+++ hosts/V14/configuration.nix
@@ -26,8 +26,17 @@
     enableGraphical = true;
   };
 
+  #SSD trimming
+  services.fstrim.enable = true;
+
   gnome.enable = true;
 
+  programs.nix-ld.enable = true;
+
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   system.stateVersion = "23.11";
