1631eddeb85ae924bc7dff81d6b1ec54695caa76
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-08-01 16:16:12 -0400

    add nixvim input

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
     let
@@ -25,10 +27,10 @@
         };
       };
       homeConfigurations = {
-          v14 = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [ ./hosts/V14/home.nix ];
-          };
+        v14 = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./hosts/V14/home.nix ];
         };
+      };
     };
 }
