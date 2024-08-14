c22ba7f6162f661f9c9fd8e48dbecd46914f9ade
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-08-14 19:00:21 -0400

    switch nixvim to stable branch

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:nix-community/nixvim";
+    nixvim.url = "github:nix-community/nixvim/nixos-24.05";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
