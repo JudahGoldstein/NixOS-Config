f00b1b7c0e7fa6c80272eea6fb6fd6ecd37e1eaf
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-08-14 18:04:47 -0400

    update update shortcut

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -2,9 +2,14 @@
 let
   myAliases = {
     "ll" = "ls -la";
+    "la" = "ls -a";
+    "l" = "ls";
+    "c" = "clear";
+    "e" = "exit";
     ".." = "cd ..";
+    "..." = "cd ../..";
     "lg" = "lazygit";
-    "nix-update" = "nh os boot --update && nh home switch --update && nh clean all --keep 3 && sudo nix-store --optimise -v"; #this is definitely not something to keep, but while getting an initial setup it's usefull
+    "nix-update" = "cd ~/NixOS-Config && nh os boot --update && nh home switch && nh clean all --keep 3 && git commit -m 'flake' flake.lock && git push"; #this is definitely not something to keep, but while getting an initial setup it's usefull. should be refacfored into a module.
   };
 in
 {
