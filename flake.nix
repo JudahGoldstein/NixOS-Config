36b1cd1d0a2c63b79df5ca8af354af4864b57436
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-08-15 17:55:33 -0400

    get p15 config started

--- flake.nix
+++ flake.nix
@@ -3,12 +3,21 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:nix-community/nixvim/nixos-24.05";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nixvim, ... }@inputs:
     let
@@ -19,7 +28,7 @@
     in
     {
       nixosConfigurations = {
-        nixos = lib.nixosSystem {
+        v14 = lib.nixosSystem {
           inherit system;
           modules = [
             ./hosts/V14/configuration.nix
@@ -31,12 +40,34 @@
             nixpkgs.config.allowUnfree = true;
           };
         };
+
+        p15 = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hosts/P15/configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+          specialArgs = {
+            inherit inputs;
+            pkgs-unstable = pkgs-unstable;
+            nixpkgs.config.allowUnfree = true;
+          };
+        };
       };
+
       homeConfigurations = {
         v14 = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
             ./hosts/V14/home.nix
+            inputs.nixvim.homeManagerModules.nixvim
+          ];
+        };
+
+        p15 = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./hosts/P15/home.nix
             inputs.nixvim.homeManagerModules.nixvim
           ];
         };
