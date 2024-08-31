d346b1fc3378742d133b6a4e1d41ad7baf58a618
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-08-31 14:28:31 -0400

    start config for home server

---
+++ hosts/HS/configuration.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./bootloader.nix
+      ../../defaultConfig.nix
+    ];
+
+  users.users.p15 = {
+    isNormalUser = true;
+    description = "Judah (HS)";
+    extraGroups = [ "networkmanager" "wheel" "dialout" "uucp" ];
+    packages = with pkgs; [ ];
+  };
+
+  programs.nh.flake = /home/HS/NixOS-Config;
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin = {
+    enable = false;
+    user = "HS";
+  };
+
+  networking.hostName = "HS";
+
+  # SSD trimming
+  services.fstrim.enable = true;
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "23.11";
+}
---
+++ hosts/HS/home.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+let
+  username = "HS";
+  homeDirectory = "/home/" + username;
+in
+{
+  imports =
+    [
+      ../../defaultHome.nix
+    ];
+
+  home.username = username;
+  home.homeDirectory = homeDirectory;
+
+  home.file = { };
+
+  home.sessionVariables = {
+    EDITOR = "micro";
+    BROWSER = "firefox";
+    TERMINAL = "kitty";
+    TERM = "kitty";
+  };
+  
+  home.stateVersion = "23.11";
+
+  programs.home-manager.enable = true;
+}
