ade82f730742939545acc1aa2b713f850a31c0d6
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-09-03 20:43:46 -0400

    figuring out nixos-anywhere

--- hosts/HS/disks.nix
+++ hosts/HS/disks.nix
@@ -1,27 +1,47 @@
-{
+{ ... }: {
   disko.devices = {
-    disk = {
-      x = {
-        type = "disk";
-        device = "/dev/sdx";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "64M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
+    disk.main = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
+          };
+          esp = {
+            name = "ESP";
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
             };
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zroot";
-              };
+          };
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "root_vg";
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      root_vg = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
             };
           };
         };
