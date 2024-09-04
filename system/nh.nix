b473b4fcece931e31827e19d7b50623775fe9288
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-09-03 21:00:54 -0400

    moving configs to standard folder

--- system/nh.nix
+++ system/nh.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   programs.nh = {
-    programs.nh.flake = /etc/nixos;
+    flake = /etc/nixos;
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep 3";
