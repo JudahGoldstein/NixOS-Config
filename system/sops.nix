4c45b6b52c022743f2b6074685c71aa62cb3b223
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-09-06 21:32:53 -0400

    sops stuff

--- system/sops.nix
+++ system/sops.nix
@@ -1,22 +1,41 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+with lib;
 {
-  environment.systemPackages = with pkgs;
-    [
-      sops
-      age
-      ssh-to-age
-    ];
-
-  sops = {
-    defaultSopsFile = /etc/nixos/secrets.yaml;
-    validateSopsFiles = false;
-
-    age = {
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      keyFile = "/var/lib/sops-nix/key.txt";
-      generateKey = true;
+  options = {
+    sops-name = mkOption {
+      type = types.str;
+      default = "";
     };
   };
 
+  config = {
+    environment.systemPackages = with pkgs;
+      [
+        sops
+        age
+        ssh-to-age
+      ];
 
+    sops = {
+      defaultSopsFile = /etc/nixos/secrets.yaml;
+      validateSopsFiles = false;
+
+      age = {
+        sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+        keyFile = "/var/lib/sops-nix/key.txt";
+        generateKey = true;
+      };
+
+      secrets = {
+        "private-keys/${config.sops-name}" = {
+          path = "/home/${config.sops-name}/.ssh/${config.sops-name}";
+          owner = "${config.sops-name}";
+        };
+        "public-keys/${config.sops-name}" = {
+          path = "/home/${config.sops-name}/.ssh/${config.sops-name}.pub";
+          owner = "${config.sops-name}";
+        };
+      };
+    };
+  };
 }
