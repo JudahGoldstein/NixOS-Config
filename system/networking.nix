a3f63852cfde7c2c9fb4792f60aa545a2d56942f
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-09-06 20:11:53 -0400

    this is part of the user set I guess

--- system/networking.nix
+++ system/networking.nix
@@ -1,10 +1,6 @@
 { config, pkgs, ... }:
 {
   services.openssh.enable = true;
-  openssh.authorizedKeys.keys =
-    [
-      (builtins.readFile ../hosts/public-keys/p15.pub)
-    ];
   networking.networkmanager.enable = true;
   services.ivpn.enable = true;
   environment.systemPackages = with pkgs; [ ivpn ];
