57c615129d791703cad7970237a5c3583b0239be
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-09-17 12:12:50 -0400

    testing nvidia on live usb

--- hosts/installer/configuration.nix
+++ hosts/installer/configuration.nix
@@ -42,6 +42,20 @@ in
       ssh-to-age
     ];
 
+  hardware.opengl = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   system.stateVersion = "23.11";
