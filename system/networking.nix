0e919a8993a6d77f3164daf6ce787a1a42fc3e18
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-02 15:07:16 -0400

    ssh troubleshooting

--- system/networking.nix
+++ system/networking.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 {
   networking.hostName = config.name;
-  services.openssh.enable = true;
   services.ssh-agent.enable = true;
   networking.networkmanager.enable = true;
   services.ivpn.enable = true;
