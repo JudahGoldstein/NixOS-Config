efa831a0d19ec6f2ecba736c61da8fc03b97270c
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-02 15:00:25 -0400

    thought this was default

--- system/networking.nix
+++ system/networking.nix
@@ -2,6 +2,7 @@
 {
   networking.hostName = config.name;
   services.openssh.enable = true;
+  services.ssh-agent.enable = true;
   networking.networkmanager.enable = true;
   services.ivpn.enable = true;
   environment.systemPackages = with pkgs; [ ivpn ];
