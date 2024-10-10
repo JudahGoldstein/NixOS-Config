18ab2dd274f4252ef64ba6baacbbde339dbcc036
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-10 12:41:37 -0400

    make media-server an optional module

--- user/media-server/media-server.nix
+++ user/media-server/media-server.nix
@@ -1,11 +1,21 @@
 { config, pkgs, lib, ... }:
 with lib;
 {
-  imports = 
+  imports = if config.media-server.enable then
   [
     ./sonarr.nix
     ./radarr.nix
     ./prowlarr.nix
     ./jellyfin.nix
-  ];
+  ] else [];
+  options = {
+    media-server.enable = mkOption {
+      default = false;
+      description = "Enable the media server services";
+    };
+  };
+
+  config = mkIf config.media-server.enable {
+    torrent.enable = true; # Enable the torrent service
+  };
 }
