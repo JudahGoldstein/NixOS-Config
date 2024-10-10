b9e71f28458d02082e0fd4b13f5c407af38f699d
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-10 12:05:05 -0400

    initiall commit for media stuff

--- user/media-server/jellyfin.nix
+++ user/media-server/jellyfin.nix
@@ -1,4 +1,12 @@
 { config, pkgs, lib, ... }:
 with lib;
 {
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+    mediaDir = "/mnt/media";
+    cacheDir = "/var/cache/jellyfin";
+    dataDir = "/var/lib/jellyfin";
+    logDir = "/var/log/jellyfin";
+  };
 }
