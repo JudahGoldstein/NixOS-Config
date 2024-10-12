cbd23602f5f6b07504f0f4843deff2c40f68d211
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-11 22:36:50 -0400

    needed move media-server stuff to system

--- system/media-server/jellyfin.nix
+++ system/media-server/jellyfin.nix
@@ -1,11 +1,10 @@
 { config, pkgs, lib, ... }:
 with lib;
 {
-  config = mkIf media-server.enable {
+  config = mkIf config.media-server.enable {
     services.jellyfin = {
       enable = true;
       openFirewall = true;
-      mediaDir = "/mnt/media";
       cacheDir = "/var/cache/jellyfin";
       dataDir = "/var/lib/jellyfin";
       logDir = "/var/log/jellyfin";
