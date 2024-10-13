c75870c0f34ddc76286d292fa7f9e0cc6e41e050
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-13 01:22:10 -0400

    sops fix

--- system/sops.nix
+++ system/sops.nix
@@ -14,7 +14,7 @@ with lib;
 
     age = {
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      keyFile = "/var/lib/sops-nix/key.txt";
+      keyFile = "/home/${config.name}/.config/sops/age/keys.txt";
       generateKey = true;
     };
 
