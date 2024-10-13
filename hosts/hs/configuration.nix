1807c7ff3fc0c9a3a000e6ee81816877b7554755
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-13 01:22:31 -0400

    mount raid array/

--- hosts/hs/configuration.nix
+++ hosts/hs/configuration.nix
@@ -20,6 +20,12 @@
   
   media-server.enable = true;
   
+  fileSystems."/mnt/media" =
+    {
+      device = "/dev/disk/by-uuid/2dfcbe87-8cbd-4363-bdb8-a4e24558e227";
+      fsType = "ext4";
+    };
+
   # SSD trimming
   services.fstrim.enable = true;
 
