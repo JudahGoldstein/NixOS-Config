a1dc8d5f4baaa925bf185806f2d69aa0f18f5fc6
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-13 06:42:24 -0400

    stash stuff

--- system/media-server/stash.nix
+++ system/media-server/stash.nix
@@ -1,20 +1,14 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, pkgs-unstable, lib, ... }:
 with lib;
 {
-  options = {
-    stash.enable = mkOption {
-      default = false;
-      description = "Enable stash.";
-    };
-  };
-  config = mkIf config.stash.enable {
-    environment.systemPackages = with pkgs;[stash];
+  config = mkIf config.media-server.enable {
+    environment.systemPackages = with pkgs-unstable;[ stash ];
     systemd.services.stash = {
       description = "Stash media server";
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
-        ExecStart = "${pkgs.stash}/bin/stash";
+        ExecStart = "${pkgs-unstable.stash}/bin/stash -c /var/lib/stash/config/config.yml";
         User = "root";
         Group = "root";
         Restart = "always";
