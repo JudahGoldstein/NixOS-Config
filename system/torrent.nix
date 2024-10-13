4a8ff532c4cd08221702f0a238761d6fe4b48bfa
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-13 01:23:25 -0400

    get torrents working

--- system/torrent.nix
+++ system/torrent.nix
@@ -12,9 +12,28 @@ with lib;
     services.rtorrent = {
       enable = true;
       openFirewall = true;
+      port = 5000;
       dataDir = "/var/lib/rtorrent";
       downloadDir = "/mnt/media/Downloads";
+      configText = ''
+        group.seeding.ratio.enable =
+        group2.seeding.ratio.min.set = 110
+        group2.seeding.ratio.max.set = 200
+      '';
     };
+
     environment.systemPackages = with pkgs;[ flood ];
+    systemd.services.flood = {
+      description = "Flood rtorrent web interface";
+      after = [ "network.target" "rtorrent.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.flood}/bin/flood";
+        User = "root";
+        Group = "root";
+        Restart = "always";
+        Environment = "NODE_ENV=production";
+      };
+    };
   };
 }
