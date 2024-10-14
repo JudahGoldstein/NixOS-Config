7f0be2302a3af18aeaf951c9c55e9b97bb349f7b
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-14 19:55:16 -0400

    finally have tls workinp properly, needs some hardening though

--- system/media-server/jellyfin.nix
+++ system/media-server/jellyfin.nix
@@ -10,17 +10,23 @@ with lib;
       logDir = "/var/log/jellyfin";
     };
 
-    services.caddy.virtualHosts."jellyfin.janjuta.duckdns.org".extraConfig = ''
-      reverse_proxy http://127.0.0.1:8096
-      tls /var/lib/acme/janjuta.duckdns.org/cert.pem /var/lib/acme/janjuta.duckdns.org/key.pem {
-        protocols tls1.3
-      }
-    '';
-    services.caddy.virtualHosts."jellyfin.jantun.duckdns.org".extraConfig = ''
-      reverse_proxy http://127.0.0.1:8096
-      tls /var/lib/acme/jantun.duckdns.org/cert.pem /var/lib/acme/jantun.duckdns.org/key.pem {
-        protocols tls1.3
-      }
-    '';
+    services.caddy.virtualHosts."jellyfin.janjuta.duckdns.org" = {
+      useACMEHost = "janjuta.duckdns.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:8096
+        tls /var/lib/acme/janjuta.duckdns.org/cert.pem /var/lib/acme/janjuta.duckdns.org/key.pem {
+          protocols tls1.3
+        }
+      '';
+    };
+    services.caddy.virtualHosts."jellyfin.jantun.duckdns.org" = {
+      useACMEHost = "jantun.duckdns.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:8096
+        tls /var/lib/acme/jantun.duckdns.org/cert.pem /var/lib/acme/jantun.duckdns.org/key.pem {
+          protocols tls1.3
+        }
+      '';
+    };
   };
 }
