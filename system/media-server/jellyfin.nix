3a3f9119f0df8802846a518916d0e009ff97e288
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-15 00:02:59 -0400

    config for intro-skipper

--- system/media-server/jellyfin.nix
+++ system/media-server/jellyfin.nix
@@ -2,6 +2,27 @@
 with lib;
 {
   config = mkIf config.media-server.enable {
+    nixpkgs.overlays = with pkgs; [
+      (
+        final: prev:
+          {
+            jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
+              installPhase = ''
+                runHook preInstall
+
+                # this is the important line
+                sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
+
+                mkdir -p $out/share
+                cp -a dist $out/share/jellyfin-web
+
+                runHook postInstall
+              '';
+            });
+          }
+      )
+    ];
+
     services.jellyfin = {
       enable = true;
       openFirewall = true;
