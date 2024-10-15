1e1fdd80c91e659b90d6dd4628d46c6c26f8b598
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-15 00:03:48 -0400

    switch from rtorrent to transmission

--- system/torrent.nix
+++ system/torrent.nix
@@ -9,36 +9,37 @@ with lib;
     };
   };
   config = mkIf config.torrent.enable {
-    services.rtorrent = {
+
+    services.transmission = {
       enable = true;
       openFirewall = true;
-      port = 5000;
-      dataDir = "/var/lib/rtorrent";
-      downloadDir = "/mnt/media/Downloads";
-      configText = ''
-        group.seeding.ratio.enable =
-        group2.seeding.ratio.min.set = 110
-        group2.seeding.ratio.max.set = 200
-      '';
+      openRPCPort = true;
+      openPeerPorts = true;
+      downloadDirPermissions = "777";
+      settings = {
+        download-dir = "/mnt/media/Downloads";
+        incomplete-dir-enabled = false;
+        watch-dir-enabled = false;
+        peer-port = 50000;
+        umask = 0;
+      };
+      settings = {
+        "rpc-host-whitelist" = "*";
+        "rpc-host-whitelist-enabled" = true;
+        "cache-size-mb" = 64;
+        "peer-limit-global" = 500;
+        "peer-limit-per-torrent" = 100;
+        "ratio-limit" = 1.1;
+        "ratio-limit-enabled" = true;
+        "idle-seeding-limit-enabled" = false;
+      };
+      credentialsFile = "/var/lib/secrets/transmission-credentials.json";
     };
 
-    environment.systemPackages = with pkgs;[ flood ];
-    systemd.services.flood = {
-      description = "Flood rtorrent web interface";
-      after = [ "network.target" "rtorrent.service" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.flood}/bin/flood";
-        User = "root";
-        Group = "root";
-        Restart = "always";
-        Environment = "NODE_ENV=production";
-      };
-    };
-    services.caddy.virtualHosts."flood.janjuta.duckdns.org" = {
+    services.caddy.virtualHosts."transmission.janjuta.duckdns.org" = {
       useACMEHost = "janjuta.duckdns.org";
       extraConfig = ''
-        reverse_proxy http://127.0.0.1:3000
+        reverse_proxy http://127.0.0.1:9091
       '';
     };
   };
