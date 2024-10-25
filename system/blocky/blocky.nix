d4d90973466ccef22d2d70ed5ff43596d30f6e6f
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-10-25 16:22:41 -0400

    update adblock lists

--- system/blocky/blocky.nix
+++ system/blocky/blocky.nix
@@ -22,16 +22,17 @@ with lib;
         };
         blocking = {
           blackLists = {
-            multi = [ "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus.txt" ];
+            multi = [ "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus.txt" "https://big.oisd.nl/domainswild" ];
             scams = [ "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/fake.txt" ];
             threatIntel = [ "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/dnsmasq/tif.txt" ];
+            mobile = [ "https://adaway.org/hosts.txt" ];
           };
           whiteLists = {
             # Piracy is bad kids
             notPiracy = [ "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/anti.piracy.txt" "https://blocklistproject.github.io/Lists/adguard/piracy-ags.txt" ];
           };
           clientGroupsBlock = {
-            default = [ "multi" "scams" "threatIntel" "blacklist" ];
+            default = [ "multi" "scams" "threatIntel" "mobile" ];
           };
         };
       };
