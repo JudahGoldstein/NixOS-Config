a2819b92283c13343769b3b69d2310225a8fae86
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-11-19 12:07:38 -0500

    boilerplate for DNS

--- system/networking.nix
+++ system/networking.nix
@@ -5,4 +5,13 @@
   environment.systemPackages = with pkgs; [ ivpn ];
   services.ivpn.enable = true;
   services.openssh.enable = true;
+
+  # networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  # services.resolved = {
+  #   enable = true;
+  #   dnssec = "true";
+  #   domains = [ "~." ];
+  #   fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  #   dnsovertls = "true";
+  # };
 }
