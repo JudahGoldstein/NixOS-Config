e69a5f0a6d85bd562a02c4122aa113a0e4d07556
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-12-01 04:18:13 -0500

    24.11 update

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
   description = "package updates";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-24.05";
+      url = "github:nix-community/nixvim/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
