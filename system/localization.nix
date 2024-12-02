161d788c46ab06f745263909dea905bae97292f9
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-12-01 23:50:34 -0500

    fixed sonarr for now

--- system/localization.nix
+++ system/localization.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 {
+  environment.systemPackages = [ pkgs.glibcLocales ];
   time.timeZone = "America/Toronto";
   i18n.defaultLocale = "en_CA.UTF-8";
   services.xserver.xkb = {
