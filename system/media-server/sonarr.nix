251862dad1a719e15b232960c3b18b76f442d7fb
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-12-01 23:50:34 -0500

    fixed sonarr for now

--- system/media-server/sonarr.nix
+++ system/media-server/sonarr.nix
@@ -8,6 +8,12 @@ with lib;
       dataDir = "/var/lib/sonarr";
       package = pkgs.sonarr;
     };
+    nixpkgs.config.permittedInsecurePackages = [
+      "aspnetcore-runtime-wrapped-6.0.36"
+      "aspnetcore-runtime-6.0.36"
+      "dotnet-sdk-wrapped-6.0.428"
+      "dotnet-sdk-6.0.428"
+    ];
     services.caddy.virtualHosts."sonarr.janjuta.duckdns.org" = {
       useACMEHost = "janjuta.duckdns.org";
       extraConfig = ''
