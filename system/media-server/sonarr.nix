a92bc33133167a697cd0da4fe8e44a378ea76330
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-12-01 22:17:28 -0500

    leaving this working and commenting the import instead

--- system/media-server/sonarr.nix
+++ system/media-server/sonarr.nix
@@ -2,17 +2,17 @@
 with lib;
 {
   config = mkIf config.media-server.enable {
-    # services.sonarr = {
-    #   enable = true;
-    #   openFirewall = true;
-    #   dataDir = "/var/lib/sonarr";
-    #   package = pkgs.sonarr;
-    # };
-    # services.caddy.virtualHosts."sonarr.janjuta.duckdns.org" = {
-    #   useACMEHost = "janjuta.duckdns.org";
-    #   extraConfig = ''
-    #     reverse_proxy http://127.0.0.1:8989
-    #   '';
-    # };
+    services.sonarr = {
+      enable = true;
+      openFirewall = true;
+      dataDir = "/var/lib/sonarr";
+      package = pkgs.sonarr;
+    };
+    services.caddy.virtualHosts."sonarr.janjuta.duckdns.org" = {
+      useACMEHost = "janjuta.duckdns.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:8989
+      '';
+    };
   };
 }
