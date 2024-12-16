b3959f8ca87b98f3a176c2dd2893c717a2b4ba45
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-12-16 12:01:21 -0500

    simplify

--- system/media-server/jellyfin.nix
+++ system/media-server/jellyfin.nix
@@ -2,26 +2,6 @@
 with lib;
 {
   config = mkIf config.media-server.enable {
-    nixpkgs.overlays = with pkgs; [
-      (
-        final: prev:
-          {
-            jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
-              installPhase = ''
-                runHook preInstall
-
-                # this is the important line
-                sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
-
-                mkdir -p $out/share
-                cp -a dist $out/share/jellyfin-web
-
-                runHook postInstall
-              '';
-            });
-          }
-      )
-    ];
 
     services.jellyfin = {
       enable = true;
@@ -29,7 +9,7 @@ with lib;
       cacheDir = "/var/cache/jellyfin";
       dataDir = "/var/lib/jellyfin";
       logDir = "/var/log/jellyfin";
-      package = pkgs-unstable.jellyfin;
+      package = pkgs.jellyfin;
     };
 
     services.caddy.virtualHosts."jellyfin.janjuta.duckdns.org" = {
