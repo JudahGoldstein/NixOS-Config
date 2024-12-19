f1923bbea8723a4561cf88da1099e8db2e51024b
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-12-19 16:34:26 -0500

    enable wayland

--- system/desktop/kde.nix
+++ system/desktop/kde.nix
@@ -10,9 +10,6 @@
   config = lib.mkIf (config.kde.enable == true) {
     services.displayManager.sddm.enable = true;
     services.desktopManager.plasma6.enable = true;
-
-    services.displayManager.defaultSession = "plasmax11";
-    services.displayManager.sddm.wayland.enable = false;
     environment.plasma6.excludePackages = with pkgs.kdePackages;
       [
         konsole
