3a8538bb2c05df8d2ff37a0224070ee801fdd2f9
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-12-24 14:42:27 -0500

    combine sh.nix

--- system/sh.nix
+++ system/sh.nix
@@ -1,6 +1,32 @@
 { config, pkgs, ... }:
+let
+  myAliases = {
+    "ll" = "ls -la";
+    "la" = "ls -a";
+    "l" = "ls";
+    "c" = "clear";
+    "e" = "exit";
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    "lg" = "lazygit";
+    "v" = "nvim";
+    "nix-update" = "cd /etc/nixos/ && git pull && nix flake update && nh os boot && nh home switch && nh clean all --keep 3 && git commit -m 'flake' flake.lock && git push";
+    "ligma" = "echo balls";
+  };
+in
 {
-  programs.zsh.enable = true;
+  programs.bash.shellAliases = myAliases;
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = myAliases;
+    ohMyZsh = {
+      enable = true;
+      custom = "/etc/nixos/user/shell/custom";
+      theme = "gruvbox";
+      plugins = [ ];
+    };
+  };
   environment.shells = with pkgs; [ zsh ];
   users.defaultUserShell = pkgs.zsh;
 }
