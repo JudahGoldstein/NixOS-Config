6e30ca0fcec628b5bc3a70496c240071ef133d92
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-12-24 15:35:14 -0500

    swap neofetch for nerdfetch

--- system/sh.nix
+++ system/sh.nix
@@ -10,6 +10,7 @@ let
     "..." = "cd ../..";
     "lg" = "lazygit";
     "v" = "nvim";
+    "neofetch" = "nerdfetch";
     "nix-update" = "cd /etc/nixos/ && git pull && nix flake update && nh os boot && nh home switch && nh clean all --keep 3 && git commit -m 'flake' flake.lock && git push";
     "ligma" = "echo balls";
   };
