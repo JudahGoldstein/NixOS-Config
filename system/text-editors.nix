5a10fb15fb3750139600a9224a9dc7d273848f60
Judah Goldstein <yehudah.lev+github@gmail.com>
2024-12-30 21:43:47 -0500

    move micro to text-editors

--- system/text-editors.nix
+++ system/text-editors.nix
@@ -13,21 +13,27 @@ with lib;
       default = true;
       description = "Enable libreoffice suite";
     };
-    
+
     qownnotes.enable = mkOption {
       type = types.bool;
       default = false;
       description = "Enable qownnotes";
     };
 
+    micro.enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable micro";
+    };
   };
 
   config = {
     environment.systemPackages = with pkgs; lib.filter (pkg: pkg != null)
       [
-        (if config.obsidian.enable then pkgs.obsidian else null)
-        (if config.libreoffice.enable then pkgs.libreoffice else null)
-        (if config.qownnotes.enable then pkgs.qownnotes else null)
+        (if config.obsidian.enable then obsidian else null)
+        (if config.libreoffice.enable then libreoffice else null)
+        (if config.qownnotes.enable then qownnotes else null)
+        (if config.micro.enable then micro else null)
         hunspell
       ];
   };
