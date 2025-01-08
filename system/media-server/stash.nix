7096b58e774c6fbea63014f4cdcdde4a7ff5aed9
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-01-07 23:37:05 -0500

    remove unused docker enable and add stash

---
+++ system/media-server/stash.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, pkgs-unstable, lib, ... }:
+with lib;
+{
+  config = mkIf config.media-server.enable {
+    environment.systemPackages = with pkgs; [ stash ];
+    networking.firewall = #stash port
+      {
+        allowedTCPPorts = [ 9049 ];
+        allowedUDPPorts = [ 9049 ];
+      };
+    systemd.services.stash = {
+      enable = true;
+      description = "stash";
+      wantedBy = [ "multi-user.target" ];
+      unitConfig.type = "simple";
+      path = [ "/mnt/media/Bulk/stash/" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.stash}/bin/stash --port 9049 -c /mnt/media/Bulk/stash/config.yml";
+        Restart = "always";
+      };
+    };
+    services.caddy.virtualHosts."stash.janjuta.duckdns.org" = {
+      useACMEHost = "janjuta.duckdns.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:9049
+      '';
+    };
+    services.caddy.virtualHosts."stash.jantun.duckdns.org" = {
+      useACMEHost = "jantun.duckdns.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:9049
+      '';
+    };
+  };
+}
