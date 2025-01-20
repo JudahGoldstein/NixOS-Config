ea4358a5e68bcc0dccc9bb6ecff834ca0a2b0776
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-01-20 14:19:03 -0500

    add ai input and fix formatting

--- flake.nix
+++ flake.nix
@@ -19,9 +19,13 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    stable-diffusion-webui-nix = {
+      url = "github:Janrupf/stable-diffusion-webui-nix/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, disko, sops-nix, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, disko, sops-nix, stable-diffusion-webui-nix, ... }@inputs:
     let
       system = "x86_64-linux";
 
@@ -37,23 +41,25 @@
         system = "x86_64-linux";
         config.allowUnfree = true;
         config.nix.channel.enable = false;
-        config.permittedInsecurePackages = [
-          "aspnetcore-runtime-wrapped-6.0.36" #sonarr
-          "aspnetcore-runtime-6.0.36"         #sonarr
-          "dotnet-sdk-wrapped-6.0.428"        #sonarr
-          "dotnet-sdk-6.0.428"                #sonarr
-        ];
+        config.permittedInsecurePackages =
+          [
+            "aspnetcore-runtime-wrapped-6.0.36" #sonarr
+            "aspnetcore-runtime-6.0.36" #sonarr
+            "dotnet-sdk-wrapped-6.0.428" #sonarr
+            "dotnet-sdk-6.0.428" #sonarr
+          ];
       };
     in
     {
       nixosConfigurations = {
         v14 = lib.nixosSystem {
           inherit system;
-          modules = [
-            ./hosts/v14/configuration.nix
-            inputs.home-manager.nixosModules.default
-            sops-nix.nixosModules.sops
-          ];
+          modules =
+            [
+              ./hosts/v14/configuration.nix
+              inputs.home-manager.nixosModules.default
+              sops-nix.nixosModules.sops
+            ];
           specialArgs = {
             inherit inputs;
             inherit pkgs-unstable;
@@ -62,11 +68,12 @@
 
         p15 = lib.nixosSystem {
           inherit system;
-          modules = [
-            ./hosts/p15/configuration.nix
-            inputs.home-manager.nixosModules.default
-            sops-nix.nixosModules.sops
-          ];
+          modules =
+            [
+              ./hosts/p15/configuration.nix
+              inputs.home-manager.nixosModules.default
+              sops-nix.nixosModules.sops
+            ];
           specialArgs = {
             inherit inputs;
             inherit pkgs-unstable;
@@ -75,12 +82,13 @@
 
         hs = lib.nixosSystem {
           inherit system;
-          modules = [
-            ./hosts/hs/configuration.nix
-            inputs.home-manager.nixosModules.default
-            sops-nix.nixosModules.sops
-            disko.nixosModules.disko
-          ];
+          modules =
+            [
+              ./hosts/hs/configuration.nix
+              inputs.home-manager.nixosModules.default
+              sops-nix.nixosModules.sops
+              disko.nixosModules.disko
+            ];
           specialArgs = {
             inherit inputs;
             inherit pkgs-unstable;
@@ -88,10 +96,11 @@
         };
 
         installer = lib.nixosSystem {
-          modules = [
-            ./hosts/installer/configuration.nix
-            sops-nix.nixosModules.sops
-          ];
+          modules =
+            [
+              ./hosts/installer/configuration.nix
+              sops-nix.nixosModules.sops
+            ];
           specialArgs = {
             inherit inputs;
             inherit pkgs-unstable;
@@ -102,32 +111,32 @@
       homeConfigurations = {
         v14 = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          modules = [
-            ./hosts/v14/home.nix
-            sops-nix.homeManagerModules.sops
-
-          ];
+          modules =
+            [
+              ./hosts/v14/home.nix
+              sops-nix.homeManagerModules.sops
+            ];
         };
 
         p15 = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          modules = [
-            ./hosts/p15/home.nix
-            sops-nix.homeManagerModules.sops
-
-          ];
+          modules =
+            [
+              ./hosts/p15/home.nix
+              sops-nix.homeManagerModules.sops
+            ];
           extraSpecialArgs = { inherit pkgs-unstable; };
         };
 
         hs = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          modules = [
-            ./hosts/hs/home.nix
-            sops-nix.homeManagerModules.sops
-          ];
+          modules =
+            [
+              ./hosts/hs/home.nix
+              sops-nix.homeManagerModules.sops
+            ];
           extraSpecialArgs = { inherit pkgs-unstable; };
         };
-
       };
     };
 }
