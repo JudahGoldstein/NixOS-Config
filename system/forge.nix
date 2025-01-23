5ec9493d04db5f9cab0dd1d88f2e409b1837c1ae
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-01-22 23:59:48 -0500

    systemd service broken

--- system/forge.nix
+++ system/forge.nix
@@ -11,18 +11,6 @@ with lib;
   config = mkIf config.forge.enable {
     nixpkgs.overlays = [ stable-diffusion-webui-nix.overlays.default ];
     environment.systemPackages = [ pkgs.stable-diffusion-webui.forge.cuda ];
-    systemd.services.forge = {
-      enable = true;
-      description = "forge webui";
-      wantedBy = [ "default.target" ];
-      unitConfig.type = "simple";
-      serviceConfig = {
-        ExecStart = "${pkgs.stable-diffusion-webui.forge.cuda}/bin/stable-diffusion-webui";
-        Restart = "always";
-        RestartSec = 30;
-        StartLimitBurst = 6;
-      };
-    };
     services.caddy.virtualHosts."forge.janjuta.duckdns.org" = mkIf config.caddy.enable {
       useACMEHost = "janjuta.duckdns.org";
       extraConfig = ''
