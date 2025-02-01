bd0977ac227be02ab46753cace683c33e9d11a94
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-02-01 02:09:37 -0500

    needs offload

--- hosts/hs/configuration.nix
+++ hosts/hs/configuration.nix
@@ -35,7 +35,7 @@
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
-    powerManagement.finegrained = true;
+    powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
