01be9b1c7f5ee7cdecb23c950d7d18f273c12a5e
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-03-16 22:17:54 -0400

    add forge systemd service

--- system/forge.nix
+++ system/forge.nix
@@ -11,6 +11,18 @@ with lib;
   config = mkIf config.forge.enable {
     nixpkgs.overlays = [ stable-diffusion-webui-nix.overlays.default ];
     environment.systemPackages = [ pkgs.stable-diffusion-webui.forge.cuda ];
+    systemd.services.forge = {
+      description = "Forge WebUI Service";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.stable-diffusion-webui.forge.cuda}/bin/stable-diffusion-webui --cuda-malloc";
+        Restart = "always";
+        RestartSec = 10;
+        User = config.name;
+        EnvironmentFile = "/home/${config.name}/env.env"; #printenv > ~/env.env
+      };
+    };
     services.caddy.virtualHosts."forge.janjuta.duckdns.org" = mkIf config.caddy.enable {
       useACMEHost = "janjuta.duckdns.org";
       extraConfig = ''
