d92b50faa42f7b2085058639cf5db50634b96b47
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-03-18 21:06:19 -0400

    add sonarr to ts

--- system/media-server/sonarr.nix
+++ system/media-server/sonarr.nix
@@ -14,5 +14,11 @@ with lib;
         reverse_proxy http://127.0.0.1:8989
       '';
     };
+    services.caddy.virtualHosts."sonarr.ts.janjuta.org" = {
+      useACMEHost = "janjuta.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:8989
+      '';
+    };
   };
 }
