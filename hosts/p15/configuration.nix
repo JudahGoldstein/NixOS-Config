c7718a860d0df3c1755568e9d1caa2b8ab89e00e
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-04-08 23:01:01 -0400

    still not working...

--- hosts/p15/configuration.nix
+++ hosts/p15/configuration.nix
@@ -37,14 +37,10 @@
   nvidia-prime.enable = true;
   nvidia-prime.offload = true;
 
-  systemd.services.disableTouchscreen = {
-    description = "Disable touchscreen";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-    };
-    script = "echo 0018:2A94:D64D.0004 | tee /sys/bus/hid/drivers/hid-multitouch/unbind";
-  };
+  #udev rule for disabling touchscreen
+  services.udev.extraRules = ''
+  ACTION!="remove", KERNEL=="event[0-9]*", ENV{ID_VENDOR_ID}=="2A94", ENV{ID_MODEL_ID}=="D64D", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+  '';
 
   gnome.enable = true;
 
