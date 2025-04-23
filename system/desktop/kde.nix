0a01279d639e38e120c561bdcdd8642e6c13a82d
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-04-22 21:50:27 -0400

    move other desktops to ly

--- system/desktop/kde.nix
+++ system/desktop/kde.nix
@@ -15,17 +15,10 @@ with lib;
       ocs-url
       ddcutil
     ];
-    services.xserver = {
-      displayManager = {
-        setupCommands = ''
-          ${pkgs.numlockx}/bin/numlockx on
-        '';
-        gdm = {
-          enable = true;
-          wayland = true;
-          autoSuspend = false;
-        };
-      };
+    services.displayManager = {
+      enable = true;
+      ly.enable = true;
+      preStart = "";
     };
     services.desktopManager.plasma6.enable = true;
     environment.plasma6.excludePackages = with pkgs.kdePackages;
