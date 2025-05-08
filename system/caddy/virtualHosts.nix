92faedbcc2631fab7f588aa657e3107dc8fb3e9b
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-05-08 19:35:29 -0400

    create caddy helper

---
+++ system/caddy/virtualHosts.nix
@@ -0,0 +1,42 @@
+{ lib, ... }:
+
+let
+  mkLocalVirtualHost = name: port: {
+    "${name}.local.janjuta.org" = {
+      useACMEHost = "janjuta.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:${toString port}
+      '';
+    };
+    "${name}.ts.janjuta.org" = {
+      useACMEHost = "janjuta.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:${toString port}
+      '';
+    };
+  };
+
+  mkPublicVirtualHost = name: port: {
+    "${name}.local.janjuta.org" = {
+      useACMEHost = "janjuta.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:${toString port}
+      '';
+    };
+    "${name}.ts.janjuta.org" = {
+      useACMEHost = "janjuta.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:${toString port}
+      '';
+    };
+    "${name}.wan.janjuta.org" = {
+      useACMEHost = "janjuta.org";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:${toString port}
+      '';
+    };
+  };
+in
+{
+  inherit mkLocalVirtualHost mkPublicVirtualHost;
+}
