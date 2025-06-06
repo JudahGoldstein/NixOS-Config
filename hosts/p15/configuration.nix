89ff2206abc4abaddcacc69b205ce8c89c32caa5
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-06-06 17:48:36 -0400

    not working

--- hosts/p15/configuration.nix
+++ hosts/p15/configuration.nix
@@ -26,15 +26,9 @@
       fsType = "ext4";
     };
 
-
   # Prime for Nvidia Optimus
   nvidia-prime.enable = true;
   nvidia-prime.offload = true;
-
-  #udev rule for disabling touchscreen
-  services.udev.extraRules = ''
-    ACTION!="remove", KERNEL=="event[0-9]*", ENV{ID_VENDOR_ID}=="2A94", ENV{ID_MODEL_ID}=="D64D", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-  '';
 
   gnome.enable = true;
 
