c8502fcec0a610b6e2fa4f65546d57edfa5f2ecc
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-06-19 22:16:50 -0400

    create forge.nix

---
+++ system/forge.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, stable-diffusion-webui-nix, lib, ... }:
+let
+  virtualHosts = import ./caddy/virtualHosts.nix { inherit lib; };
+in
+with lib;
+{
+  options = {
+    services.sd-webui-forge.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable the Stable Diffusion WebUI Forge service.";
+    };
+  };
+  config = {
+    services.sd-webui-forge = {
+      enable = true;
+      user = "sd-webui-forge";
+      group = "sd-webui-forge";
+      dataDir = "/var/lib/sd-webui-forge";
+      package = pkgs.stable-diffusion-webui.forge.cuda;
+      listen = true;
+      port = 7860;
+      extraArgs = "--cuda-malloc";
+    };
+
+    services.caddy.virtualHosts = (virtualHosts.mkLocalVirtualHost "forge" 7860);
+  };
+}
