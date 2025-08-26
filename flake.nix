b99be0e1beb9c676942111925bd1bbef1f0f0567
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-08-26 16:34:59 -0400

    Update pin

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-24.11";
-    # nixpkgs-pin.url = "github:nixos/nixpks/1fd8bada0b6117e6c7eb54aad5813023eed37ccb";
-    nixpkgs-pin.url = "nixpkgs/nixos-unstable";
+    nixpkgs-pin.url = "github:nixos/nixpks/20075955deac2583bb12f07151c2df830ef346b4";
+    # nixpkgs-pin.url = "nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
