8ce165247ff15d68c26abe5b9017ea2ef75f1f26
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-08-29 16:50:09 -0400

    refactor secret exports

--- system/sops.nix
+++ system/sops.nix
@@ -11,57 +11,65 @@ with lib;
   environment.variables = {
     SOPS_EDITOR = "micro";
   };
-  sops = {
-    defaultSopsFile = "/etc/nixos/secrets.yaml";
-    validateSopsFiles = false;
+  sops =
+    {
+      defaultSopsFile = "/etc/nixos/secrets.yaml";
+      validateSopsFiles = false;
 
-    age = {
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      keyFile = "/root/.config/sops/age/keys.txt";
-      generateKey = true;
-    };
+      age = {
+        sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+        keyFile = "/root/.config/sops/age/keys.txt";
+        generateKey = true;
+      };
 
-    secrets = {
-      "private-keys/github" = {
-        path = "/home/${config.name}/.ssh/github";
-        owner = "${config.name}";
-      };
-      "public-keys/github" = {
-        path = "/home/${config.name}/.ssh/github.pub";
-        owner = "${config.name}";
-      };
-      "private-keys/${config.name}" = {
-        path = "/home/${config.name}/.ssh/${config.name}";
-        owner = "${config.name}";
-      };
-      "public-keys/p15" =
-        if config.name == "p15" then {
-          path = "/home/${config.name}/.ssh/${config.name}.pub";
-          owner = "${config.name}";
-        } else { };
-      "public-keys/hs" =
-        if config.name == "hs" then {
-          path = "/home/${config.name}/.ssh/${config.name}.pub";
-          owner = "${config.name}";
-        } else { };
-      "duckdns-token" =
-        if config.caddy.enable == true then {
-          path = "/var/lib/secrets/duckdns-token";
-          owner = config.services.caddy.user;
-          group = config.services.caddy.group;
-        } else { };
-      "cloudflare-dns-api-key" =
-        if config.caddy.enable == true then {
-          path = "/var/lib/secrets/cloudflare-dns-api-key";
-          owner = config.services.caddy.user;
-          group = config.services.caddy.group;
-        } else { };
-      "couchdb-creds" =
-        if config.services.couchdb.enable == true then {
-          path = "/var/lib/secrets/couchdb-creds";
-          owner = config.services.couchdb.user;
-          group = config.services.couchdb.group;
-        } else { };
+      secrets = lib.mkMerge [
+        ({
+          # keypairs
+          "private-keys/github" = {
+            path = "/home/${config.name}/.ssh/github";
+            owner = "${config.name}";
+          };
+          "public-keys/github" = {
+            path = "/home/${config.name}/.ssh/github.pub";
+            owner = "${config.name}";
+          };
+          "private-keys/${config.name}" = {
+            path = "/home/${config.name}/.ssh/${config.name}";
+            owner = "${config.name}";
+          };
+          "public-keys/${config.name}" = lib.mkForce {
+            path = "/home/${config.name}/.ssh/${config.name}.pub";
+            owner = "${config.name}";
+          };
+          # api keys and tokens
+          "cloudflare-dns-api-key" =
+            if config.caddy.enable == true then {
+              path = "/var/lib/secrets/cloudflare-dns-api-key";
+              owner = config.services.caddy.user;
+              group = config.services.caddy.group;
+            } else { };
+          "couchdb-creds" =
+            if config.services.couchdb.enable == true then {
+              path = "/var/lib/secrets/couchdb-creds";
+              owner = config.services.couchdb.user;
+              group = config.services.couchdb.group;
+            } else { };
+        })
+        ({
+          # authorized keys
+          "public-keys/p15" = {
+            path = "/home/${config.name}/.ssh/authorized_keys/p15.pub";
+            owner = "${config.name}";
+          };
+          "public-keys/hs" = {
+            path = "/home/${config.name}/.ssh/authorized_keys/hs.pub";
+            owner = "${config.name}";
+          };
+          "public-keys/connectbot" = {
+            path = "/home/${config.name}/.ssh/authorized_keys/connectbot.pub";
+            owner = "${config.name}";
+          };
+        })
+      ];
     };
-  };
 }
