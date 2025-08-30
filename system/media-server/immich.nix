0876d3668852e3b5dfc11ed01a68e8b42af73bdd
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-08-30 16:11:28 -0400

    update immich

--- system/media-server/immich.nix
+++ system/media-server/immich.nix
@@ -5,16 +5,16 @@ in
 with lib;
 {
   config = mkIf config.media-server.enable {
-
     services.immich = {
       enable = true;
-      port = 2283;
+      port = 58278;
+      host = "0.0.0.0";
       openFirewall = true;
       machine-learning.enable = false;
       accelerationDevices = null;
       package = pkgs.immich;
       settings = null;
     };
-    services.caddy.virtualHosts = (virtualHosts.mkPublicVirtualHost "images" 2283);
+    services.caddy.virtualHosts = (virtualHosts.mkPublicVirtualHost "immich" 58278);
   };
 }
