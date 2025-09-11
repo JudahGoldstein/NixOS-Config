0bc0cca1a438777d2c24f5588b8c4d7f1718490c
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-09-11 16:57:02 -0400

    add calibre to media server

---
+++ system/media-server/library.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, lib, ... }:
+let
+  virtualHosts = import ../caddy/virtualHosts.nix { inherit lib; };
+in
+with lib;
+{
+  config = mkIf config.media-server.enable {
+    services.calibre-web = {
+      enable = true;
+      openFirewall = true;
+      options = {
+        calibreLibrary = "/mnt/media/library/";
+        enableBookUploading = true;
+      };
+      listen = {
+        ip = "0.0.0.0";
+        port = 51852;
+      };
+    };
+    services.caddy.virtualHosts = (virtualHosts.mkLocalVirtualHost "library" 51852);
+  };
+}
