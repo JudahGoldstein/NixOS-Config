e4af5da97c5e99d1f49a34dd5c9751e5070cb2f2
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-09-13 13:55:44 -0400

    Revert "enable native gnome rdp" no multi-user support
    This reverts commit 88530da8b4e5f19717915091407a3f59d00786eb.

--- system/desktop/gnome.nix
+++ system/desktop/gnome.nix
@@ -6,11 +6,6 @@
       default = false;
       description = "Enable GNOME desktop environment.";
     };
-    gnome.remote = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Enable remote access feature for GNOME.";
-    };
   };
 
   config = lib.mkIf (config.gnome.enable == true) {
@@ -62,11 +57,5 @@
       enable = true;
       terminal = "kitty";
     };
-    services.gnome.gnome-remote-desktop.enable = true;
-
-    systemd.services.gnome-remote-desktop = {
-      wantedBy = [ "graphical.target" ];
-    };
-    networking.firewall.allowedTCPPorts = [ 3389 ];
   };
 }
