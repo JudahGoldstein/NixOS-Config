4396b7bbc3544209c69c65c5e12d1e647f12f6cc
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-09-15 12:35:22 -0400

    factor out common home

--- hosts/hs/home.nix
+++ hosts/hs/home.nix
@@ -4,20 +4,4 @@
     [
       ../../defaultHome.nix
     ];
-
-  home.username = config.name;
-  home.homeDirectory = "/home/${config.name}";
-
-  home.file = { };
-
-  home.sessionVariables = {
-    EDITOR = "micro";
-    BROWSER = "firefox";
-    TERMINAL = "kitty";
-    TERM = "kitty";
-  };
-  
-  home.stateVersion = "23.11";
-
-  programs.home-manager.enable = true;
 }
--- hosts/p15/home.nix
+++ hosts/p15/home.nix
@@ -4,22 +4,4 @@
     [
       ../../defaultHome.nix
     ];
-
-  home.username = config.name;
-  home.homeDirectory = "/home/${config.name}";
-
-  home.file = { };
-
-  home.sessionVariables = {
-    EDITOR = "micro";
-    BROWSER = "firefox";
-    TERMINAL = "kitty";
-    TERM = "kitty";
-  };
-
-  gnome.enable = true;
-
-  home.stateVersion = "23.11";
-
-  programs.home-manager.enable = true;
 }
