b46c4a9f8629a607fc1b4beb5ae1e00ee078b381
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-09-17 17:37:30 -0400

    testing copyparty

---
+++ system/media-server/copyparty.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, lib, ... }:
+let
+  virtualHosts = import ../caddy/virtualHosts.nix { inherit lib; };
+in
+with lib;
+{
+  config = mkIf config.media-server.enable {
+    services.copyparty = {
+      enable = true;
+      settings = {
+        i = "0.0.0.0";
+        p = 3923;
+      };
+      accounts = { test.passwordFile = "/etc/nixos/hosts/p15/test"; };
+      volumes = { };
+      package = (pkgs.copyparty.override {
+        withCertgen = true;
+        withThumbnails = true;
+        withFastThumbnails = true;
+        withMediaProcessing = true;
+        withHashedPasswords = true;
+      });
+    };
+    services.caddy.virtualHosts = (virtualHosts.mkLocalVirtualHost "copyparty" 3923);
+  };
+}
