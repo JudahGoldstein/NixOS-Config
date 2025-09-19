35a00279006a0c6e2e45eb8671fcb05cb1af8839
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-09-19 16:15:24 -0400

    make ssh settings more explicit

--- system/ssh.nix
+++ system/ssh.nix
@@ -5,8 +5,12 @@
   ];
   services.openssh = {
     enable = true;
-    settings.PasswordAuthentication = false;
-    settings.KbdInteractiveAuthentication = false;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      AllowUsers = [ config.name ];
+      PermitRootLogin = "no";
+    };
     authorizedKeysFiles = [ "/home/${config.name}/.ssh/authorized_keys/*" ];
   };
 }
