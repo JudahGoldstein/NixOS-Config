697ab28668e20d6b8b08122683eab8c0b6561365
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-09-21 19:27:38 -0400

    copyparty customization
    arst

--- system/media-server/copyparty.nix
+++ system/media-server/copyparty.nix
@@ -11,6 +11,11 @@ with lib;
         i = "0.0.0.0";
         p = 3923;
         rproxy = 1;
+        j = 0; #number of cores to use (0 = all)
+        no-crt = true;
+        e2dsa = true; #search
+        e2ts = true; #metadata search
+        th-r-vips = "avif,exr,fit,fits,fts,gif,hdr,heic,heics,heif,heifs,jp2,jpeg,jpg,jpx,jxl,nii,pfm,pgm,png,ppm,svg,tif,tiff,webp,pdf"; #thumbnailing with vips
       };
       accounts = { admin.passwordFile = config.sops.secrets."copyparty-password".path; };
       volumes = {
@@ -24,6 +29,7 @@ with lib;
       package = (pkgs.copyparty.override {
         withThumbnails = true;
         withFastThumbnails = true;
+        withFTP = false;
       });
     };
     environment.systemPackages = [ pkgs.copyparty ];
