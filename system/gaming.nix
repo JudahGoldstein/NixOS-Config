6243222202d5d33e96762280fcd74f10cc7eb49c
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-10-16 13:46:34 -0400

    add some more gaming options
    arst
    
    a

--- system/gaming.nix
+++ system/gaming.nix
@@ -9,18 +9,23 @@
     steam.hosting = inputs.lib.mkOption {
       type = inputs.lib.types.bool;
       default = false;
-      description = "Open ports in the firewall for steam";
+      description = "Open ports in the firewall for steam hosting";
     };
   };
 
   config = inputs.lib.mkIf config.steam.enable {
-    programs.steam = {
-      enable = true;
-      protontricks.enable = true;
-      extraCompatPackages = [ pkgs.proton-ge-bin ];
-      remotePlay.openFirewall = config.steam.hosting;
-      dedicatedServer.openFirewall = config.steam.hosting;
-      localNetworkGameTransfers.openFirewall = config.steam.hosting;
+    programs = {
+      steam = {
+        enable = true;
+        protontricks.enable = true;
+        extraCompatPackages = [ pkgs.proton-ge-bin ];
+        remotePlay.openFirewall = config.steam.hosting;
+        dedicatedServer.openFirewall = config.steam.hosting;
+        localNetworkGameTransfers.openFirewall = config.steam.hosting;
+        gamescopeSession.enable = true;
+      };
+      gamemode.enable = true;
     };
+    environment.systemPackages = with pkgs; [ mangohud ];
   };
 }
