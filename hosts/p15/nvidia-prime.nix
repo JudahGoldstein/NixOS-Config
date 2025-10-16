3e6c62cb0b425be7e4b117d3d232751758ecfd6d
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-10-16 13:44:07 -0400

    simplify nvidia config with a specialisation
    arst

--- hosts/p15/nvidia-prime.nix
+++ hosts/p15/nvidia-prime.nix
@@ -1,37 +1,26 @@
 { config, pkgs, ... }@inputs:
 {
-  options = {
-    nvidia-prime.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = false;
-      description = "Enable nvidia";
-    };
-    nvidia-prime.sync = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = false;
-      description = "Enable nvidia sync mode";
-    };
-    nvidia-prime.offload = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = false;
-      description = "Enable nvidia offload mode";
-    };
-  };
-  config = inputs.lib.mkIf config.nvidia-prime.enable {
+  config = {
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia = {
       open = false;
       modesetting.enable = true;
       prime = {
-        sync = inputs.lib.mkIf config.nvidia-prime.sync {
-          enable = true;
-        };
-        offload = inputs.lib.mkIf config.nvidia-prime.offload {
+        offload = {
           enable = true;
           enableOffloadCmd = true;
         };
         nvidiaBusId = "PCI:01:00:0";
         intelBusId = "PCI:00:02:0";
+      };
+    };
+    specialisation.gaming.configuration = {
+      hardware.nvidia.prime = {
+        offload = {
+          enable = inputs.lib.mkForce false;
+          enableOffloadCmd = inputs.lib.mkForce false;
+        };
+        sync.enable = inputs.lib.mkForce true;
       };
     };
   };
