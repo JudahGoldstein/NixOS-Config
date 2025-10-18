2e8de2875863389b137fe4743bb21156202a4684
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-10-18 13:57:23 -0400

    mirror the options nixConf for initial installs

--- nixConf.nix
+++ nixConf.nix
@@ -1,17 +1,14 @@
 { config, pkgs, ... }@inputs:
 {
   nix.settings = {
-    substituters =
-      [
-        "https://nix-community.cachix.org/"
-      ];
-    trusted-public-keys =
-      [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
     experimental-features = [ "nix-command" "flakes" ];
-    auto-optimise-store = true;
     download-buffer-size = 536870912; # 512MiB
+    auto-optimise-store = true;
+    substituters = [
+      "https://nix-community.cachix.org/"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
-
 }
