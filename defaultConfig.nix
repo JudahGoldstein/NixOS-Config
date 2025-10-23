a43be74df1a3e2282edf0a7de29a98197f8c1a62
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-10-23 01:14:18 -0400

    manually enabling while I diagnose a build failure

--- defaultConfig.nix
+++ defaultConfig.nix
@@ -53,7 +53,24 @@
     stylix.enable = true;
     stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
     stylix.polarity = "dark";
-    hm.stylix.targets.firefox.profileNames = [ "default" ];
+    hm.stylix.targets = {
+      firefox = {
+        enable = true;
+        profileNames = [ "default" ];
+      };
+      kitty.enable = true;
+      gnome.enable = true;
+      qt.enable = true;
+      lazygit.enable = true;
+      micro.enable = true;
+      obsidian.enable = true;
+      nixos-icons.enable = true;
+      vesktop.enable = true;
+      eog.enable = true;
+      gnome-text-editor.enable = true;
+      vscode.enable = true;
+    };
+    stylix.autoEnable = false;
 
     hm = {
       home.username = config.name;
