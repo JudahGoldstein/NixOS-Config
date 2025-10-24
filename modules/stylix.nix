f3dd9bdd8274646f6e92b13038a3c8d4e68d0fa3
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-10-24 17:42:26 -0400

    disable Qt theming to avoid qgnomeplatform compile error

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -3,22 +3,5 @@
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
   stylix.polarity = "dark";
-  hm.stylix.targets = {
-    firefox = {
-      enable = true;
-      profileNames = [ "default" ];
-    };
-    kitty.enable = true;
-    gnome.enable = true;
-    qt.enable = true;
-    lazygit.enable = true;
-    micro.enable = true;
-    obsidian.enable = true;
-    nixos-icons.enable = true;
-    vesktop.enable = true;
-    eog.enable = true;
-    gnome-text-editor.enable = true;
-    vscode.enable = true;
-  };
-  stylix.autoEnable = false;
+  stylix.targets.qt.enable = false; # Disable system-level Qt theming to avoid qgnomeplatform compile error
 }
