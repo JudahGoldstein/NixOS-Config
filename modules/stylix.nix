9f824b90632f3180449cc3993f953fd92b49e507
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-11-06 11:29:01 -0500

    add package to set for local caching

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }@inputs:
 {
+  environment.systemPackages = [ pkgs.base16-schemes ];
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
   stylix.polarity = "dark";
