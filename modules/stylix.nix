51dc3e44cdeb9cb470a7b66ef0875ba11ef9230a
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-11-06 11:28:14 -0500

    fixed upstream

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -3,5 +3,4 @@
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
   stylix.polarity = "dark";
-  stylix.targets.qt.enable = false; # Disable system-level Qt theming to avoid qgnomeplatform compile error
 }
