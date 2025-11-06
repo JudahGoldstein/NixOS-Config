a2de3ca9c91ecbc3153b994e0033d18546b3e893
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-11-06 11:29:55 -0500

    change stylix base16Scheme

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -2,6 +2,6 @@
 {
   environment.systemPackages = [ pkgs.base16-schemes ];
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/evenok-dark.yaml";
   stylix.polarity = "dark";
 }
