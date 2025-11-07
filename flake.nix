0eabc9741659e02c2523110db474d33c904e85cb
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-11-07 10:23:33 -0500

    add devenv cache explicitly

--- flake.nix
+++ flake.nix
@@ -9,10 +9,12 @@
     extra-substituters = [
       "https://nix-community.cachix.org/"
       "https://chaotic-nyx.cachix.org/"
+      "https://devenv.cachix.org/"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
   };
 
