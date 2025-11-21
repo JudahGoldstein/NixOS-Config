fe7e8de8402e4438d27d8db3eb243ad5585a27f1
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-11-21 14:38:11 -0500

    simplify av config

--- modules/audio-video.nix
+++ modules/audio-video.nix
@@ -7,22 +7,10 @@
       description = "Enable easyeffects";
     };
 
-    audacity.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = false;
-      description = "Enable audacity";
-    };
-
     vlc.enable = inputs.lib.mkOption {
       type = inputs.lib.types.bool;
       default = true;
       description = "Enable VLC";
-    };
-
-    shotcut.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = false;
-      description = "Enable shotcut";
     };
 
     ffmpeg.enable = inputs.lib.mkOption {
@@ -37,15 +25,7 @@
       with pkgs;
       inputs.lib.filter (pkg: pkg != null) [
         (if config.easyeffects.enable then easyeffects else null)
-        (
-          if config.easyeffects.enable then
-            if config.gnome.enable then gnomeExtensions.easyeffects-preset-selector else null
-          else
-            null
-        )
-        (if config.audacity.enable then audacity else null)
         (if config.vlc.enable then vlc else null)
-        (if config.shotcut.enable then shotcut else null)
         (if config.ffmpeg.enable then ffmpeg else null)
       ];
   };
