4ca33e9e5b0ac582bab4366e6caa997bf2771a4e
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-12-02 16:40:15 -0500

    factor common code

--- helpers/virtualHosts.nix
+++ helpers/virtualHosts.nix
@@ -1,41 +1,23 @@
 inputs:
 let
   inherit (inputs) lib;
-  mkLocalVirtualHost = name: port: {
-    "${name}.local.janjuta.org" = {
-      useACMEHost = "janjuta.org";
-      extraConfig = ''
-        reverse_proxy http://127.0.0.1:${toString port}
-      '';
-    };
-    "${name}.ts.janjuta.org" = {
-      useACMEHost = "janjuta.org";
-      extraConfig = ''
-        reverse_proxy http://127.0.0.1:${toString port}
-      '';
-    };
-  };
+  mkVirtualHost = domains: port: lib.genAttrs domains (domain: {
+    useACMEHost = "janjuta.org";
+    extraConfig = ''
+      reverse_proxy http://127.0.0.1:${toString port}
+    '';
+  });
 
-  mkPublicVirtualHost = name: port: {
-    "${name}.local.janjuta.org" = {
-      useACMEHost = "janjuta.org";
-      extraConfig = ''
-        reverse_proxy http://127.0.0.1:${toString port}
-      '';
-    };
-    "${name}.ts.janjuta.org" = {
-      useACMEHost = "janjuta.org";
-      extraConfig = ''
-        reverse_proxy http://127.0.0.1:${toString port}
-      '';
-    };
-    "${name}.wan.janjuta.org" = {
-      useACMEHost = "janjuta.org";
-      extraConfig = ''
-        reverse_proxy http://127.0.0.1:${toString port}
-      '';
-    };
-  };
+  mkLocalVirtualHost = name: port: mkVirtualHost [
+    "${name}.local.janjuta.org"
+    "${name}.ts.janjuta.org"
+  ] port;
+
+  mkPublicVirtualHost = name: port: mkVirtualHost [
+    "${name}.local.janjuta.org"
+    "${name}.ts.janjuta.org"
+    "${name}.wan.janjuta.org"
+  ] port;
 in
 {
   inherit mkLocalVirtualHost mkPublicVirtualHost;
