1848fdb600619dfcd876eed4fc323177fc01acbc
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-12-06 13:52:50 -0500

    simplify
    simplify

--- modules/audio-video.nix
+++ modules/audio-video.nix
@@ -1,32 +1,12 @@
 { config, pkgs, ... }@inputs:
 {
-  options = {
-    easyeffects.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = true;
-      description = "Enable easyeffects";
-    };
 
-    vlc.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = true;
-      description = "Enable VLC";
-    };
-
-    ffmpeg.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = true;
-      description = "Enable ffmpeg";
-    };
-  };
-
-  config = {
-    environment.systemPackages =
-      with pkgs;
-      inputs.lib.filter (pkg: pkg != null) [
-        (if config.easyeffects.enable then easyeffects else null)
-        (if config.vlc.enable then vlc else null)
-        (if config.ffmpeg.enable then ffmpeg else null)
-      ];
+  environment.systemPackages = with pkgs; [
+    vlc
+    ffmpeg-headless
+  ];
+  hm.services.easyeffects = {
+    enable = true;
+    package = pkgs.easyeffects;
   };
 }
