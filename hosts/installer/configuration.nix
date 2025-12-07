0695690dd0a18e078c75c65b4a4e29591411fbfe
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-12-07 14:26:07 -0500

    make installer stand-alone

--- hosts/installer/configuration.nix
+++ hosts/installer/configuration.nix
@@ -10,15 +10,51 @@ in
   imports = [
     "${inputs.modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
     "${inputs.modulesPath}/installer/cd-dvd/channel.nix"
-    ../../modules/gnome.nix
   ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
 
   services.openssh.enable = true;
-  gnome.enable = true;
   networking.networkmanager.enable = true;
   networking.wireless.enable = false;
+
+  services.desktopManager.gnome.enable = true;
+  services.libinput.enable = true;
+  services.displayManager = {
+    enable = true;
+    ly = {
+      enable = true;
+      settings = {
+        allow_empty_password = false;
+        clear_password = true;
+        hide_version_string = true;
+        numlock = true;
+      };
+      x11Support = false;
+    };
+    preStart = "";
+  };
+  environment.gnome.excludePackages = with pkgs; [
+    eog
+    epiphany
+    gedit
+    simple-scan
+    totem
+    yelp
+    evince
+    file-roller
+    geary
+    seahorse
+    gnome-calculator
+    gnome-clocks
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    gnome-weather
+    gnome-tour
+  ];
 
   users.users.nixos = {
     password = "arst";
@@ -26,8 +62,7 @@ in
     hashedPasswordFile = inputs.lib.mkForce null;
     initialPassword = inputs.lib.mkForce null;
     initialHashedPassword = inputs.lib.mkForce null;
-    openssh.authorizedKeys.keys = 
-    [
+    openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfe84n5+9IQpH1dhqOgoJrPcNeHZKHYziYFIWYZt+F7 yehudah.lev@gmail.com"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBIXo+wrGI4775CY4wT2LbuoWigKKjiKWu8S6EWzdn/6 yehudah.lev@gmail.com"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ6pp2JPSB1FeDX/AyOISOKvZe4YDuPP//9GmzZar518 yehudah.lev@gmail.com"
