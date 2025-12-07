b104b058937427b1acbac3251a51db1e3771e96d
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-12-07 10:32:33 -0500

    add cuda binary cache

--- modules/nixConf.nix
+++ modules/nixConf.nix
@@ -18,11 +18,13 @@
       "https://nix-community.cachix.org/"
       "https://chaotic-nyx.cachix.org/"
       "https://devenv.cachix.org/"
+      "https://cache.nixos-cuda.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
   };
 }
