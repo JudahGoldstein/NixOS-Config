e4828a2fdec148e0f16b24b99a9cc9587782f76a
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-12-07 14:54:08 -0500

    v14 Install

--- hosts/v14/bootloader.nix
+++ hosts/v14/bootloader.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }@inputs:
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+    timeout = 10;
+  };
 }
---
+++ hosts/v14/disks.nix
@@ -0,0 +1,33 @@
+{ ... }:
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
