10701e58fefdbab670b076859db2975c5dafc516
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-12-08 12:58:46 -0500

    facter for p15
    fixup

--- hosts/p15/configuration.nix
+++ hosts/p15/configuration.nix
@@ -4,11 +4,6 @@
 
   steam.enable = true;
 
-  fileSystems."/bulk" = {
-    device = "/dev/disk/by-uuid/5ef0952b-b607-40dc-9479-9007dea4252c";
-    fsType = "ext4";
-  };
-
   boot.kernelPackages = pkgs.linuxPackages_cachyos;
 
   softColemak.enable = true;
