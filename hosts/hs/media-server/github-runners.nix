80b012eb0a1a0bb7bb6cb6916acce3e3dfc28030
Judah Goldstein <yehudah.lev+github@gmail.com>
2025-12-31 22:21:11 -0500

    user should be generated by runner

--- hosts/hs/media-server/github-runners.nix
+++ hosts/hs/media-server/github-runners.nix
@@ -8,13 +8,8 @@
       extraLabels = [ "self-hosted" "linux" "x64" "nixos" ];
       noDefaultLabels = true;
       url = "https://github.com/JudahGoldstein/NixOS-Config";
-      user = "github-runner-hs";
     };
   };
-  users.users.github-runner-hs = {
-    isSystemUser = true;
-    group = "nogroup";
-    description = "GitHub Runner User for HS Media Server";
-  };  
+  
   nix.settings.trusted-users = [ config.name "github-runner-hs" ];
 }
