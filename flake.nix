49e86284db35608b37ed7048dff02e51271bc946
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-01-10 03:54:00 -0500

    update binary caches
    q

--- flake.nix
+++ flake.nix
@@ -10,13 +10,17 @@
       "https://nix-community.cachix.org/"
       "https://cache.nixos-cuda.org"
       "https://numtide.cachix.org"
-      "https://om.cachix.org/"
+	  "https://zed.cachix.org"
+	  "https://cache.nixos.asia/oss" #for om in gh actions
+	  "https://om.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "om.cachix.org-1:ifal/RLZJKN4sbpScyPGqJ2+appCslzu7ZZF/C01f2Q="
+	  "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
+	  "oss:KO872wNJkCDgmGN3xy9dT89WAhvv13EiKncTtHDItVU="
+	  "om.cachix.org-1:ifal/RLZJKN4sbpScyPGqJ2+appCslzu7ZZF/C01f2Q="
     ];
   };
 
--- modules/nixConf.nix
+++ modules/nixConf.nix
@@ -15,15 +15,19 @@
     auto-optimise-store = true;
     substituters = [
       "https://nix-community.cachix.org/"
-      "https://chaotic-nyx.cachix.org/"
       "https://cache.nixos-cuda.org"
       "https://numtide.cachix.org"
+      "https://zed.cachix.org"
+	  "https://cache.nixos.asia/oss"
+	  "https://om.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
+	  "oss:KO872wNJkCDgmGN3xy9dT89WAhvv13EiKncTtHDItVU="
+	  "om.cachix.org-1:ifal/RLZJKN4sbpScyPGqJ2+appCslzu7ZZF/C01f2Q="
     ];
   };
 }
