2ecf4b4acd2405605e385a70f178f7c4184e1b88
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-01-13 17:32:01 -0500

    clean up

--- modules/dev.nix
+++ modules/dev.nix
@@ -1,55 +1,53 @@
 { config, pkgs, ... }@inputs:
 {
-  config = inputs.lib.mkIf config.dev.enable {
-    hm.programs.opencode.enable = true;
-    hm.programs.zed-editor = {
-      enable = true;
-      package = inputs.pkgs-zed.zed-editor;
-      extensions = [
-        "nix"
-        "github-actions"
-        "mcp-server-github"
-        "opencode"
-      ];
-      extraPackages = with pkgs; [
-        nixd
-        nixfmt
-      ];
-      mutableUserKeymaps = true;
-      mutableUserSettings = true;
-      mutableUserTasks = true;
-      userSettings = {
-        telemetry = {
-          diagnostics = false;
-          metrics = true;
-        };
-        auto_update = false;
-        agent = {
-          "single_file_review" = true;
-        };
-        lsp = {
-          nixd = {
-            settings = {
-              diagnostic.suppress = [
-                "sema-unused-def-lambda-witharg-arg"
-                "sema-unused-def-lambda-noarg-formal"
-                "sema-unused-def-let"
-              ];
-              nixpkgs = {
-                expr = "import <nixpkgs> {}";
-              };
-              autoArchive = true;
-            };
-            initialization_options.formatting.command = [ "nixfmt" ];
-          };
-        };
-        languages = {
-          Nix = {
-            language_servers = [
-              "nixd"
-              "!nil"
+  hm.programs.opencode.enable = true;
+  hm.programs.zed-editor = {
+    enable = true;
+    package = inputs.pkgs-zed.zed-editor;
+    extensions = [
+      "nix"
+      "github-actions"
+      "mcp-server-github"
+      "opencode"
+    ];
+    extraPackages = with pkgs; [
+      nixd
+      nixfmt
+    ];
+    mutableUserKeymaps = true;
+    mutableUserSettings = true;
+    mutableUserTasks = true;
+    userSettings = {
+      telemetry = {
+        diagnostics = false;
+        metrics = true;
+      };
+      auto_update = false;
+      agent = {
+        "single_file_review" = true;
+      };
+      lsp = {
+        nixd = {
+          settings = {
+            diagnostic.suppress = [
+              "sema-unused-def-lambda-witharg-arg"
+              "sema-unused-def-lambda-noarg-formal"
+              "sema-unused-def-let"
             ];
+            nixpkgs = {
+              expr = "import <nixpkgs> {}";
+            };
+            autoArchive = true;
           };
+          initialization_options.formatting.command = [ "nixfmt" ];
+        };
+      };
+      languages = {
+        Nix = {
+          language_servers = [
+            "nixd"
+            "!nil"
+          ];
         };
       };
     };
