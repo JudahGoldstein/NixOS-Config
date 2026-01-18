7471093bba124ed7e0962e19f0a9436e8451eaa8
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-01-18 18:22:01 -0500

    add virtualhost

--- hosts/hs/media-server/attic.nix
+++ hosts/hs/media-server/attic.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }@inputs:
+let
+  virtualHosts = import ../../../helpers/virtualHosts.nix inputs;
+in
 {
   services.atticd = {
     enable = true;
     environmentFile = "/var/lib/secrets/attic-key";
+    settings.listen = "127.0.0.1:6277";
   };
+  services.caddy.virtualHosts = (virtualHosts.mkLocalVirtualHost "attic" 6277);
 }
