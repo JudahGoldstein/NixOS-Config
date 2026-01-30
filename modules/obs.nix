219db1acbb8c391ec258e74bf177378dea987c61
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-01-29 19:37:12 -0500

    simplify obs config

--- modules/obs.nix
+++ modules/obs.nix
@@ -1,25 +1,17 @@
 { config, pkgs, ... }@inputs:
 {
-  options = {
-    obs.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = false;
-      description = "Enable the OBS Studio.";
-    };
-  };
-
-  config = inputs.lib.mkIf config.obs.enable {
-    boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
-    boot.kernelModules = [
-      "v4l2loopback"
+  boot.extraModulePackages = inputs.lib.mkIf config.programs.obs-studio.enable [
+    config.boot.kernelPackages.v4l2loopback
+  ];
+  boot.kernelModules = inputs.lib.mkIf config.programs.obs-studio.enable [
+    "v4l2loopback"
+  ];
+  programs.obs-studio = {
+    enable = inputs.lib.mkDefault false;
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-backgroundremoval
+      input-overlay
+      obs-source-clone
     ];
-    programs.obs-studio = {
-      enable = true;
-      plugins = with pkgs.obs-studio-plugins; [
-        obs-backgroundremoval
-        input-overlay
-        obs-source-clone
-      ];
-    };
   };
 }
