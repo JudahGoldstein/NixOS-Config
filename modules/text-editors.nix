7af2edd60239eb675b5cf75e5ecbfa31f157ac1c
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-01-29 19:59:12 -0500

    remove text editor custom options

--- modules/text-editors.nix
+++ modules/text-editors.nix
@@ -1,34 +1,10 @@
 { config, pkgs, ... }@inputs:
 {
-  options = {
-    obsidian.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = true;
-      description = "Enable obsidian";
-    };
-
-    libreoffice.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = true;
-      description = "Enable libreoffice suite";
-    };
-
-    micro.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = true;
-      description = "Enable micro";
-    };
-  };
-
-  config = {
-    environment.systemPackages =
-      with pkgs;
-      inputs.lib.filter (pkg: pkg != null) [
-        (if config.obsidian.enable then obsidian else null)
-        (if config.libreoffice.enable then libreoffice-fresh else null)
-        (if config.micro.enable then micro else null)
-        hunspell
-        pdftk
-      ];
-  };
+  environment.systemPackages = with pkgs; [
+    obsidian
+    libreoffice-fresh
+    micro
+    hunspell
+    pdftk
+  ];
 }
