7af91b86fb02702443ed10d7202f74ddf4a326a9
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-01-29 19:23:03 -0500

    simplify antivirus config

--- modules/antivirus.nix
+++ modules/antivirus.nix
@@ -1,27 +1,18 @@
 { config, pkgs, ... }@inputs:
 {
-  options = {
-    clamav.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = false;
-      description = "Enable Clam Antivirus";
-    };
-  };
-  config = {
-    services.clamav = inputs.lib.mkIf config.clamav.enable {
-      daemon.enable = true;
-      fangfrisch.enable = true;
-      updater.enable = true;
-      scanner.enable = true;
-      scanner.interval = "*-*-* 06:00:00";
-      scanner.scanDirectories = [
-        "/home"
-        "/var/lib"
-        "/tmp"
-        "/etc"
-        "/var/tmp"
-        "/mnt/"
-      ];
-    };
+  services.clamav = {
+    daemon.enable = inputs.lib.mkDefault false;
+    fangfrisch.enable = config.services.clamav.daemon.enable;
+    updater.enable = config.services.clamav.daemon.enable;
+    scanner.enable = config.services.clamav.daemon.enable;
+    scanner.interval = "*-*-* 06:00:00";
+    scanner.scanDirectories = [
+      "/home"
+      "/var/lib"
+      "/tmp"
+      "/etc"
+      "/var/tmp"
+      "/mnt/"
+    ];
   };
 }
