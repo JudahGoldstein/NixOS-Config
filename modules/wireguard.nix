2c2e24e9684b91d0d27a1c2ad840c8c009414093
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-01-29 20:00:18 -0500

    remove custom tailscale option

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -1,20 +1,12 @@
 { config, pkgs, ... }@inputs:
 {
-  options = {
-    tailscale.enable = inputs.lib.mkOption {
-      default = true;
-      description = "Enable wireguard host";
-    };
-  };
-  config = inputs.lib.mkIf config.tailscale.enable {
-    services.tailscale = {
-      enable = true;
-      openFirewall = true;
-      disableTaildrop = true;
-      useRoutingFeatures = inputs.lib.mkDefault "client";
-      extraSetFlags = inputs.lib.mkDefault [
-        "--accept-dns=false"
-      ];
-    };
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+    disableTaildrop = true;
+    useRoutingFeatures = inputs.lib.mkDefault "client";
+    extraSetFlags = inputs.lib.mkDefault [
+      "--accept-dns=false"
+    ];
   };
 }
