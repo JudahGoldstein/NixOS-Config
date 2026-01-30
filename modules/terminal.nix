0ddbbb2201233ad5f70b66e0c993cd6b3be586ee
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-01-29 19:55:17 -0500

    remove custom terminal options

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,33 +1,19 @@
 { config, pkgs, ... }@inputs:
 {
-  options = {
-    kitty.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = true;
-    };
-    terminal-recording.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = true;
-    };
+  environment.systemPackages = with pkgs; [
+    asciinema
+    asciinema-agg
+    termshot
+  ];
+  programs.starship = {
+    enable = true;
+    presets = [ "no-runtime-versions" ];
   };
-  config = {
-    environment.systemPackages =
-      with pkgs;
-      inputs.lib.mkIf config.terminal-recording.enable [
-        asciinema
-        asciinema-agg
-        termshot
-      ];
-    programs.starship = {
-      enable = true;
-      presets = [ "no-runtime-versions" ];
-    };
-    programs.nautilus-open-any-terminal = inputs.lib.mkIf config.services.desktopManager.gnome.enable {
-      enable = true;
-      terminal = "kitty";
-    };
-    hm.programs.kitty = {
-      enable = config.kitty.enable;
-    };
+  programs.nautilus-open-any-terminal = inputs.lib.mkIf config.services.desktopManager.gnome.enable {
+    enable = true;
+    terminal = "kitty";
+  };
+  hm.programs.kitty = {
+    enable = true;
   };
 }
