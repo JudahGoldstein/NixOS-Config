55bc4efcac27f02cac1df5e8e707715f4bc8664b
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-01-29 19:33:32 -0500

    simplify logitech config

--- modules/logitech.nix
+++ modules/logitech.nix
@@ -1,17 +1,7 @@
 { config, pkgs, ... }@inputs:
 {
-  options = {
-    logitech.enable = inputs.lib.mkOption {
-      type = inputs.lib.types.bool;
-      default = true;
-      description = "Enable logitech wireless customization/support";
-    };
-  };
-
-  config = inputs.lib.mkIf config.logitech.enable {
-    hardware.logitech.wireless = {
-      enable = true;
-      enableGraphical = true;
-    };
+  hardware.logitech.wireless = {
+    enable = true;
+    enableGraphical = true;
   };
 }
