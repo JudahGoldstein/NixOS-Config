c5d9aea0414b5f278434160b219ae233cc90adbc
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-02-03 15:04:55 -0500

    increase maximum nar-info size

--- hosts/hs/modules/services/attic.nix
+++ hosts/hs/modules/services/attic.nix
@@ -16,7 +16,7 @@
     settings = {
       listen = "127.0.0.1:6277";
       database.url = "postgresql:///atticd?host=/run/postgresql";
-
+      max-nar-info-size = 5 * 1048576; # 5 MiB
       chunking = {
         nar-size-threshold = 64 * 1024; # 64 KiB
         min-size = 16 * 1024; # 16 KiB
