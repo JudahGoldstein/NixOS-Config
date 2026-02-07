04373d97e178c43636abfbd4a8aa893fa41734ba
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-02-07 17:55:05 -0500

    Change Stylix scheme to material-darker

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -2,7 +2,7 @@
 {
   environment = inputs.lib.mkIf config.stylix.enable { systemPackages = [ pkgs.base16-schemes ]; };
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/evenok-dark.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/material-darker.yaml";
   stylix.polarity = "dark";
   stylix.targets.qt.platform = inputs.lib.mkForce "qtct";
   hm.qt.platformTheme = inputs.lib.mkIf config.stylix.enable { name = inputs.lib.mkForce "adwaita"; };
