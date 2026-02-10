c898efd738be3d51d2d97dfcee029c75b66b85a9
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-02-09 19:12:42 -0500

    add cursor theme

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -1,9 +1,16 @@
 { config, pkgs, ... }@inputs:
 {
   environment = inputs.lib.mkIf config.stylix.enable { systemPackages = [ pkgs.base16-schemes ]; };
-  stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/material-darker.yaml";
-  stylix.polarity = "dark";
-  stylix.targets.qt.platform = inputs.lib.mkForce "qtct";
+  stylix = {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/material-darker.yaml";
+    cursor = {
+      name = "Quintom_Ink";
+      size = 24;
+      package = pkgs.quintom-cursor-theme;
+    };
+    polarity = "dark";
+    targets.qt.platform = inputs.lib.mkForce "qtct";
+  };
   hm.qt.platformTheme = inputs.lib.mkIf config.stylix.enable { name = inputs.lib.mkForce "adwaita"; };
 }
