0eec34e9a3dd19cc640c2562ad5c6145ac458f7c
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-02-09 19:14:22 -0500

    fixing xdg autostart

--- modules/system/display-manager.nix
+++ modules/system/display-manager.nix
@@ -10,6 +10,21 @@
         clear_password = true;
         hide_version_string = true;
         numlock = true;
+        setup_cmd =
+          let
+            xsession-wrapper =
+              pkgs.runCommand "xsession-wrapper-fixed"
+                {
+                  src = config.services.displayManager.sessionData.wrapper;
+                }
+                ''
+                  cp --preserve=mode $src $out
+                  substituteInPlace $out --replace-fail \
+                    "KDE|GNOME|Pantheon|X-NIXOS-SYSTEMD-AWARE" \
+                    "KDE|GNOME|Pantheon|X-NIXOS-SYSTEMD-AWARE|niri"
+                '';
+          in
+          "${xsession-wrapper}";
       };
       x11Support = false;
     };
