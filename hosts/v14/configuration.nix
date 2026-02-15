43958874434db686f524a7b8abbd4fb5909be8d3
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-02-15 13:13:20 -0500

    set scale on v14

--- hosts/v14/configuration.nix
+++ hosts/v14/configuration.nix
@@ -6,7 +6,13 @@
     enable = true;
     interface = "wlp2s0";
   };
-
+  hm.programs.niri.settings.outputs = {
+    "eDP-1" = {
+      position.x = 0;
+      position.y = 0;
+      scale = 1.0;
+    };
+  };
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   softColemak.enable = true;
