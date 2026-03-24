2acd4c9ca6c121674bd04765a5cc2c580a0a7721
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-03-24 14:03:44 -0400

    trying the efi approach

--- hosts/hs/bootloader.nix
+++ hosts/hs/bootloader.nix
@@ -1,17 +1,28 @@
 { config, pkgs, ... }@inputs:
 {
-  boot.loader = {
-    systemd-boot.enable = false;
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot";
+  boot = {
+    loader = {
+      systemd-boot.enable = false;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub = {
+        enable = true;
+        device = "nodev";
+        useOSProber = false;
+        efiSupport = true;
+        copyKernels = true;
+        extraFiles = {
+          "netbootxyz.efi" = "${pkgs.netbootxyz-efi}";
+        };
+        extraEntries = ''
+          menuentry "netbootxyz" {
+            chainloader /netbootxyz.efi
+          }
+        '';
+      };
     };
-    grub = {
-      enable = true;
-      device = "nodev";
-      useOSProber = false;
-      efiSupport = true;
-      copyKernels = true;
-    };
+    initrd.verbose = false;
   };
 }
