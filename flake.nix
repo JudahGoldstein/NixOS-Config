c5ba46184384da4a2d834ba5a54020683a72517f
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-03-30 10:19:33 -0400

    add noctalia cache

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
       "https://cache.nixos.asia/oss" # for om in gh actions
       "https://om.cachix.org"
       "https://niri.cachix.org"
+	  "https://noctalia.cachix.org"
     ];
     extra-trusted-public-keys = [
       "personal-cache:v1TL7gi821LiCxDKSqtvAq07b0KptIUPfAL7qtvWHR8="
@@ -25,6 +26,7 @@
       "oss:KO872wNJkCDgmGN3xy9dT89WAhvv13EiKncTtHDItVU="
       "om.cachix.org-1:ifal/RLZJKN4sbpScyPGqJ2+appCslzu7ZZF/C01f2Q="
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+	  "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
   };
 
