7ad8d0da0a1513078042a08db55b3a2cce6048f9
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-06-08 17:47:39 -0400

    new cache

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     download-buffer-size = 536870912; # 512MiB
     auto-optimise-store = true;
     extra-substituters = [
-      "https://attic.ts.janjuta.org/personal-cache"
+      "https://attic.ts.janjuta.org/personal-cache2"
       "https://nix-community.cachix.org/"
       "https://cache.nixos-cuda.org"
       "https://numtide.cachix.org"
@@ -18,7 +18,7 @@
       "https://noctalia.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "personal-cache:v1TL7gi821LiCxDKSqtvAq07b0KptIUPfAL7qtvWHR8="
+      "personal-cache2:DCvNwO/KOCx+jtvGrTyf9mSiInpt5HWnvwgvrPhc+eE="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
