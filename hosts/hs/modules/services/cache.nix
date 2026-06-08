e670561ae0e441deca342240d74ab9f3b40da8c8
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-06-08 19:57:15 -0400

    update harmonia limits

--- hosts/hs/modules/services/cache.nix
+++ hosts/hs/modules/services/cache.nix
@@ -5,6 +5,12 @@
     cache = {
       enable = true;
       signKeyPaths = [ "/var/lib/secrets/harmonia.secret" ];
+      settings = {
+        bind = "[::]:5000";
+        workers = 8;
+        max_connection_rate = 512;
+        priority = 30;
+      };
     };
   };
   services.caddy.virtualHosts = (inputs.virtualHosts.mkLocalVirtualHost "cache" 5000);
