bbe404f155b91b9b579bc5ec17e13f331728c26b
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-07-03 12:44:05 -0400

    bodge fix for outdated pnpm until fixed upstream

--- modules/programs/discord.nix
+++ modules/programs/discord.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }@inputs:
 {
+  nixpkgs.overlays = [
+    (final: _prev: {
+      pnpm_10_29_2 = final.pnpm_10;
+    })
+  ];
   hm.programs.vesktop = {
     enable = true;
     package = pkgs.vesktop;
