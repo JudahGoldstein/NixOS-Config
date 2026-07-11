10fe53d4bf24133531df40e39935a69a7af06be4
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-07-11 18:21:29 -0400

    update obs config

--- modules/programs/obs.nix
+++ modules/programs/obs.nix
@@ -1,17 +1,17 @@
 { config, pkgs, ... }@inputs:
 {
-  boot.extraModulePackages = inputs.lib.mkIf config.programs.obs-studio.enable [
-    config.boot.kernelPackages.v4l2loopback
-  ];
-  boot.kernelModules = inputs.lib.mkIf config.programs.obs-studio.enable [
-    "v4l2loopback"
-  ];
   programs.obs-studio = {
     enable = inputs.lib.mkDefault false;
+    enableVirtualCamera = config.programs.obs-studio.enable;
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
     plugins = with pkgs.obs-studio-plugins; [
-      obs-backgroundremoval
-      input-overlay
       obs-source-clone
+      wlrobs
+      obs-backgroundremoval
     ];
   };
 }
