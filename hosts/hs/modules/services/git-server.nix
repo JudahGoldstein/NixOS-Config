5bf7725b266b9ff478232dca928cdc033c42a52b
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-07-16 15:21:42 -0400

    simplify

--- hosts/hs/modules/services/git-server.nix
+++ hosts/hs/modules/services/git-server.nix
@@ -2,7 +2,10 @@
 {
   services.gitea = {
     enable = true;
-    settings.server.DISABLE_SSH = true;
+    settings.server = {
+      DISABLE_SSH = true;
+      HTTP_PORT = 6982;
+    };
   };
   services.caddy.virtualHosts = (inputs.virtualHosts.mkLocalVirtualHost "git" 6982);
 }
