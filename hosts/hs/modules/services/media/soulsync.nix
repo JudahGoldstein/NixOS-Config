6c40460e5c539a498d8b63946f4ddeb832971eb6
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-07-18 19:17:09 -0400

    make explicit

--- hosts/hs/modules/services/media/soulsync.nix
+++ hosts/hs/modules/services/media/soulsync.nix
@@ -10,7 +10,7 @@
 
   # Containers
   virtualisation.oci-containers.containers."soulsync-webui" = {
-    image = "nezreka/soulsync:latest";
+    image = "ghcr.io/nezreka/soulsync:latest";
     environment = {
       "PUID" = "${toString (config.users.users.music.uid)}";
       "PGID" = "${toString (config.users.groups.media.gid)}";
