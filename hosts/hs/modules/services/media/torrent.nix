fcb5e3f02f6d730d82c344f1fe19b9545a92adf5
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-07-25 22:50:54 -0400

    trying out qbit
    fix

--- hosts/hs/modules/services/media/torrent.nix
+++ hosts/hs/modules/services/media/torrent.nix
@@ -22,7 +22,25 @@
       );
     };
   };
-  services.caddy.virtualHosts = (
+  services.qbittorrent = {
+    enable = true;
+    group = "media";
+    openFirewall = true;
+    torrentingPort = 32085;
+    webuiPort = 9786;
+    serverConfig = {
+      LegalNotice.Accepted = true;
+      Preferences = {
+        WebUI = {
+          Username = "user";
+          Password_PBKDF2 = "6SVZBBGBrDa5kzDnAdmskQ==:Q2TAacuLNVLUtKiGWXZ1vzpFO+4+ZMyK9HjD6K3HIKuNseeICKgzcTSoBF0MltULgfGBiBS/APBpK2AO8ACi1Q==";
+        };
+        General.Locale = "en";
+      };
+    };
+  };
+
+  services.caddy.virtualHosts =
     inputs.virtualHosts.mkLocalVirtualHost "deluge" config.services.deluge.web.port
-  );
+    // inputs.virtualHosts.mkLocalVirtualHost "qbittorrent" config.services.qbittorrent.webuiPort;
 }
