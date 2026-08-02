504700e41b04df5f78c3dcd8b5c184369184ed18
Judah Goldstein <yehudah.lev+github@gmail.com>
2026-08-02 11:58:48 -0400

    unused keybinds

--- modules/system/niri.nix
+++ modules/system/niri.nix
@@ -202,60 +202,6 @@
         "Mod+Shift+WheelScrollUp" = {
           action.focus-workspace-up = [ ];
         };
-        "Mod+1" = {
-          action.focus-workspace = 1;
-        };
-        "Mod+2" = {
-          action.focus-workspace = 2;
-        };
-        "Mod+3" = {
-          action.focus-workspace = 3;
-        };
-        "Mod+4" = {
-          action.focus-workspace = 4;
-        };
-        "Mod+5" = {
-          action.focus-workspace = 5;
-        };
-        "Mod+6" = {
-          action.focus-workspace = 6;
-        };
-        "Mod+7" = {
-          action.focus-workspace = 7;
-        };
-        "Mod+8" = {
-          action.focus-workspace = 8;
-        };
-        "Mod+9" = {
-          action.focus-workspace = 9;
-        };
-        "Mod+Shift+1" = {
-          action.move-column-to-workspace = 1;
-        };
-        "Mod+Shift+2" = {
-          action.move-column-to-workspace = 2;
-        };
-        "Mod+Shift+3" = {
-          action.move-column-to-workspace = 3;
-        };
-        "Mod+Shift+4" = {
-          action.move-column-to-workspace = 4;
-        };
-        "Mod+Shift+5" = {
-          action.move-column-to-workspace = 5;
-        };
-        "Mod+Shift+6" = {
-          action.move-column-to-workspace = 6;
-        };
-        "Mod+Shift+7" = {
-          action.move-column-to-workspace = 7;
-        };
-        "Mod+Shift+8" = {
-          action.move-column-to-workspace = 8;
-        };
-        "Mod+Shift+9" = {
-          action.move-column-to-workspace = 9;
-        };
         "Mod+BracketLeft" = {
           action.consume-or-expel-window-left = [ ];
         };
